650ee3054e199e689013acb5b81142d673e6161f
Jerene Yang <jyang@xcalar.com>
2014-07-04 19:49:29 -0700

    Revert css changes until we are ready

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // JavaScript Document
 // Menu Bar JS
 var selectedTab = 0;
-var numTabs = 3;
+var numTabs = 7;
+var tableRowIndex = 1;
+
+function setTabs() {
+	var i;
+	for (i = 1; i<=numTabs; i++) {
+		dataSourceMouseOver(i);
+	}
+	dataSourceClick(1);
+}
 
 function loadMainContent(op) {
   $("#leftFrame").load(op.concat('_l.html'));
@@ -17,13 +26,23 @@ function dataSourceMouseOver(tabNumber) {
   var vanishLeft = "tab" + (tabNumber-1) + "r";
   
   if (tabNumber != 1) {
-	  document.getElementById(left).src = "images/light_l.png";
-	  document.getElementById(vanishLeft).src = "images/light_m.png";
+	  if (tabNumber-1 == selectedTab) {
+		  document.getElementById(left).src = "images/white_m.png";
+	  } else {
+		  document.getElementById(vanishLeft).src = "images/light_m.png";
+		  document.getElementById(left).src = "images/light_l.png";
+	  }
   }
   document.getElementById(center).style.backgroundImage = "url('images/light_m.png')";
-  document.getElementById(right).src = "images/light_r.png";
-  if (tabNumber != 3) {
-    document.getElementById(vanishRight).src = "images/light_m.png";
+  if (tabNumber != numTabs) {
+    if (tabNumber+1 == selectedTab) {
+	   document.getElementById(right).src = "images/white_m.png";
+    } else {
+		document.getElementById(vanishRight).src = "images/light_m.png";
+       document.getElementById(right).src = "images/light_r.png";
+    }
+  } else {
+	  document.getElementById(right).src = "images/light_r.png";
   }
 }
 
@@ -85,7 +104,7 @@ function dataSourceClick(tabNumber) {
   
   // Enable mouseover and mouseout for other elements
   var index;
-  for (index = 1; index <= 3; index++) {
+  for (index = 1; index <= numTabs; index++) {
 	  if (index != tabNumber) {
 		  var tabName = "tab" + index + "c";
 		  var func1 = "dataSourceMouseOver(" + index + ");";
@@ -94,6 +113,66 @@ function dataSourceClick(tabNumber) {
 		  document.getElementById(tabName).onmouseout = new Function(func2);
 		  document.getElementById(tabName).onmouseout();
 	  }
-	  
   }
+}
+
+function generateTabs() {
+	var i;
+	document.write('<td class="dataSourceMenu" id="tab1c"\
+    onMouseOver="dataSourceMouseOver(1);"\
+    onMouseOut="dataSourceMouseOut(1);"\
+    onClick="dataSourceClick(1);">\
+    &nbsp;&nbsp;DataSource-1&nbsp;&nbsp;\
+    </td>\
+    <td class="dataSourceMenu">\
+      <img src="images/dark_l.png" height="25px" id="tab2l">\
+    </td>');
+	for (i = 2; i<=numTabs; i++) {
+		document.write('\
+	      <td class="dataSourceMenu">\
+            <img src="images/dark_r.png" height="25px" id="tab'+(i-1)+'r">\
+          </td>\
+          <td class="dataSourceMenu" id="tab'+i+'c"\
+            onMouseOver="dataSourceMouseOver('+i+');"\
+            onMouseOut="dataSourceMouseOut('+i+');"\
+            onClick="dataSourceClick('+i+');">\
+            &nbsp;&nbsp;DataSource-'+i+'&nbsp;&nbsp;\
+          </td>');
+		  if (i < numTabs) {
+           document.write('<td class="dataSourceMenu">\
+                            <img src="images/dark_l.png" height="25px" id="tab'+(i+1)+'l">\
+                           </td>');
+		  } else {
+			  document.write('<td class="dataSourceMenu">\
+                  		     <img src="images/dark_r.png" height="25px" id="tab'+i+'r">\
+              					</td>');
+		  }
+	}
+}
+
+function generateBar(width, text, page) {
+	document.write('<td width="'+width+'" height="40" align="center" class="helvertica_grey"\
+                    onMouseOver="this.bgColor=\'#d55401\'"\
+                    onMouseOut="this.bgColor=\'#e77e23\'"\
+                    bgcolor="#e77e23">\
+                    <a class="menuItems" onClick="loadMainContent(\'' + page + '\')">'+
+					  text + '</a></td>');
+}
+
+function resetAutoIndex() {
+	tableRowIndex = 1;
+}
+
+function generateRowWithAutoIndex(text) {
+	$("#autoGenTable tr:last").after('<tr><td height="17" align="center" bgcolor="#FFFFFF" class="monacotype">'
+                    +tableRowIndex+'</td><td height="17" bgcolor="#FFFFFF" class="monacotype">'
+                    +text+'</td></tr>');
+	tableRowIndex++;
+}
+
+function tgenerateRowWithAutoIndex(text) {
+	$("#tautoGenTable tr:last").after('<tr><td height="17" align="center" bgcolor="#FFFFFF" class="monacotype">'
+                    +tableRowIndex+'</td><td height="17" bgcolor="#FFFFFF" class="monacotype">'
+                    +text+'</td></tr>');
+	tableRowIndex++;
 }
