0d4dee41ba96b9919582f6fe59bc1b0beaac68f2
Jerene Yang <jyang@xcalar.com>
2015-02-23 17:59:06 -0800

    GUI-447 Splash screen

--- js/login.js
+++ js/login.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    $("#loginContainer").hide();
+    var stage = new swiffy.Stage(document.getElementById('swiffycontainer'),
+                                 swiffyobject, {  });
+    stage.start();
+    setTimeout(function() {
+        $('#swiffycontainer').fadeOut(1000, function() {
+            $("#loginContainer").show();
+        });
+    }, 4500);
+    
     $('#loginButton').click(function() {
         console.log("username: "+$("#loginNameBox").val());
         console.log("password: "+$("#loginPasswordBox").val());
