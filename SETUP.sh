53f88c0a86c6131c8f9a115050b821b91b6a4a8c
Cheng Zhang <czhang@xcalar.com>
2015-04-13 16:22:35 -0700

    GUI-743 RUME.sh and SETUP.sh for configuration

--- SETUP.sh
+++ SETUP.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+HOST=localhost
+PORT=9090
+echo "Start Xcalar Insight Configuration..."
+echo "Install necessary package..."
+# install less if not exist
+if [ ! -e /usr/bin/lessc ]
+then
+    echo "do not have less, install less..."
+    sudo apt-get update && sudo apt-get install node-less
+fi
+
+# install apache if not exist
+if [ ! -e /etc/apache2 ]
+then
+    echo "do not have apache2, install apache2..."
+    sudo apt-get update && sudo apt-get install apache2
+fi
 
 # set up xcalar file
 echo "Set up Xcalar Insight..."
@@ -30,3 +47,22 @@ then
         echo "XcalarApi.js differs"
     fi
 fi
+
+# set up config.js
+cp js/sample-config.js js/config.js
+sed -i "s/<yourHostName>/$HOST/" js/config.js
+sed -i "s/9090/$PORT/" js/config.js
+cat js/config.js
+
+# set up apache
+echo "Set up apache..."
+echo "ServerName localhost" | sudo tee /etc/apache2/conf-available/fqdn.conf && sudo a2enconf fqdn
+sudo service apache2 reload
+
+sudo cp /etc/apache2/sites-available/000-default.conf  XI.conf
+sed -i 's,/var/www/html,/var/www/xcalar-gui/,' XI.conf
+sudo mv XI.conf /etc/apache2/sites-available/
+sudo mkdir -p /var/www/xcalar-gui
+sudo cp -r . /var/www/xcalar-gui/.
+sudo a2dissite 000-default && sudo a2ensite XI
+sudo service apache2 restart
