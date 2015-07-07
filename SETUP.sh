114f6c987ba69063c0bd15e96ee2ace63895b535
Jerene Yang <jyang@xcalar.com>
2015-07-07 16:08:38 -0700

    GUI-1483 Update Setup.sh

--- SETUP.sh
+++ SETUP.sh
@@ -62,7 +62,6 @@ sudo service apache2 reload
 sudo cp /etc/apache2/sites-available/000-default.conf  XI.conf
 sed -i 's,/var/www/html,/var/www/xcalar-gui/,' XI.conf
 sudo mv XI.conf /etc/apache2/sites-available/
-sudo mkdir -p /var/www/xcalar-gui
-sudo cp -r . /var/www/xcalar-gui/.
+sudo ln -s ~/xcalar-gui /var/www/xcalar-gui
 sudo a2dissite 000-default && sudo a2ensite XI
 sudo service apache2 restart
