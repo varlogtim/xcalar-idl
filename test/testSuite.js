b042e538776da71be82ecf981aa2d05ce8a83ad1
Jerene Yang <jyang@xcalar.com>
2015-07-15 00:35:33 -0700

    GUI-1533 More of Cheng's flight test

--- test/testSuite.js
+++ test/testSuite.js
@@ -13,6 +13,11 @@ window.TestSuite = (function($, TestSuite) {
     var testCases = new Array();
     var disableIsPass = true;
 
+    var fakeClick = {type: "mouseup",
+                     which: 1};
+    var fakeEnter = {type: "keypress",
+                     which: 13};
+
     TestSuite.start = function(deferred, testNameLocal, currentTestNumberLocal,
                                timeout) {
     }
@@ -77,7 +82,7 @@ window.TestSuite = (function($, TestSuite) {
             deferred = deferred.then(
                 // Need to trap the value of testCase and ii
                 (function trapFn(testCase, currentTestNumber) {
-                    return function() {
+                    return (function() {
                         var localDeferred = $.Deferred();
                         if (testCase.testCaseEnabled) {
                             console.log("====================Test ", currentTestNumber, " Begin====================");
@@ -95,7 +100,7 @@ window.TestSuite = (function($, TestSuite) {
                         }
 
                         return localDeferred.promise();
-                    }
+                    });
                 })(testCases[ii], ii + 1) // Invoking trapFn
             );
         }
@@ -119,53 +124,113 @@ window.TestSuite = (function($, TestSuite) {
     function flightTest(deferred, testName, currentTestNumber) {
         /** This test replicates a simple version of Cheng's flight demo
         This tests all major functionality
+        TEST MUST BE DONE ON A CLEAN WORKSHEET!
         It does the following:
         2. Loads 2 datasets (flight and airports)
         3. Maps flight:delay str to int
         4. Filter delay_int by > 0
         5. Upload custom n clause cat pyExec
-        6. Run pyExec on year month and day columnes
+        6. Run pyExec on year month and day columns
         7. Join with airports table
         8. Index on airlines
         9. GroupBy average on delay
         10. Aggregate on groupBy table to count number of unique airlines
         */
 
-        var dsName = "flight"+Math.floor(Math.random()*100);
- 
-        flightTestPart1(dsName);
-        function flightTestPart1(dsName) {
+        var dsName1 = "flight"+Math.floor(Math.random()*100);
+        var dsName2 = "airport"+Math.floor(Math.random()*100);
+
+        flightTestPart1(dsName1, dsName2);
+        function flightTestPart1(dsName1, dsName2) {
             $("#dataStoresTab").click();
+
+            // Import flight dataset
             $("#importDataButton").click();
             $("#filePath").val("file:///netstore/users/jerene/flight"+
                                "/airlines_2007.log");
-            $("#fileName").val(dsName);
+            $("#fileName").val(dsName1);
             $("#fileFormat .iconWrapper .icon").click();
             $("#fileFormat li[name='CSV']").click();
             $("#csvPromoteCheckbox .icon").click();
             $("#fieldDelim .icon").click();
             $("#fieldDelim .list li[name='comma']").click();
             $("#importDataSubmit").click();
+            
+            $("#importDataButton").click();
+            $("#filePath").val("file:///netstore/users/jerene/flight"+
+                               "/airports.csv");
+            $("#fileName").val(dsName2);
+            $("#fileFormat .iconWrapper .icon").click();
+            $("#fileFormat li[name='CSV']").click();
+            $("#fieldDelim .icon").click();
+            $("#fieldDelim .list li[name='comma']").click();
+            $("#importDataSubmit").click();
             setTimeout(function() {
-                flightTestPart2(dsName);
-            }, 2000); // Load should be <2s
-        }
+                flightTestPart2(dsName1, dsName2);
+            }, 6000); // Load should be <2s
+         }
         
-        function flightTestPart2(dsName) {
-            $("#dataset-"+dsName+" .gridIcon").click();
-            $("#selectDSCols .icon").click();
-            $("#selectedTable-"+dsName+" li .closeIcon")[0].click();
-            $("#selectedTable-"+dsName+" li .closeIcon")[0].click();
-            $("#selectedTable-"+dsName+" li .closeIcon")[0].click();
-            $("#selectedTable-"+dsName+" li .closeIcon")[0].click();
-            $("#selectedTable-"+dsName+" li .closeIcon")[0].click();
-            $("#submitDSTablesBtn").click();
+         function flightTestPart2(dsName1, dsName2) {
+            $("#dataset-"+dsName2+" .gridIcon").click();
             setTimeout(function() {
-                flightTestPart3(dsName);
-            }, 2000); // Index should be <2s
+                $("#selectDSCols .icon").click();
+                $("#dataset-"+dsName1+" .gridIcon").click();
+                setTimeout(function() {
+                    $("#selectDSCols .icon").click();
+                    console.log(dsName1);
+                    $("#selectedTable-"+dsName1+" li .closeIcon")[0].click();
+                    $("#selectedTable-"+dsName1+" li .closeIcon")[0].click();
+                    $("#selectedTable-"+dsName1+" li .closeIcon")[0].click();
+                    $("#selectedTable-"+dsName1+" li .closeIcon")[0].click();
+                    $("#selectedTable-"+dsName1+" li .closeIcon")[0].click();
+                    $("#submitDSTablesBtn").click();
+                    setTimeout(function() {
+                        flightTestPart3();
+                    }, 2000); // Index should be <2s
+                }, 1000);
+            }, 1000);
         }
 
-        function flightTestPart3(dsName) {
+        function flightTestPart3() {
+            var $header = $($(".flexWrap.flex-mid input[value='ArrDelay']")[0]);
+            $header.parent().parent().find(".flex-right .innerBox").mousedown();
+            var $colMenu = $("#colMenu0 .changeDataType");
+            $colMenu.mouseover();
+            $colMenu.find(".type-integer").trigger(fakeClick);
+            setTimeout(function() {
+                flightTestPart4();
+            }, 4000);
+        }
+
+        function flightTestPart4() {
+            var $header = $($(".flexWrap.flex-mid"+
+                              " input[value='ArrDelay_integer']")[0]);
+            $header.parent().parent().find(".flex-right .innerBox").mousedown();
+            var $colMenu = $("#colMenu0 .filter");
+            $colMenu.trigger(fakeClick);
+            setTimeout(function() {
+                $("#functionList input").val("gt");
+                $("#functionList input").trigger(fakeEnter);
+                $($(".argumentTable tr")[2]).find("input").val("0");
+                $("#operationsModal .modalBottom .confirm").click();
+                setTimeout(function() {
+                    flightTestPart5();
+                }, 5000);
+            }, 1000);
+        }
+
+       function flightTestPart5() {
+            $("#udfBtn").click();
+            $("#udf-tabs div[data-tab='udf-fnSection'] .label").click();
+            var textArea = $("#udf-codeArea")[0];
+            var editor = CodeMirror.fromTextArea(textArea);
+            editor.setValue('def ymd(year, month, day):\n'+
+                            '    if int(month) < 10:\n'+
+                            '        month = "0" + month\n'+
+                            '    if int(day) < 10:\n'+
+                            '        day = "0" + day\n'+
+                            '    return year + month + day');
+
             TestSuite.pass(deferred, testName, currentTestNumber);
         }
     }
