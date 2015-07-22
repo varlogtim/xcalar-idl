d29c8a5c7f4f529c11ed71b844c0368374957b87
Cheng Zhang <czhang@xcalar.com>
2015-07-22 14:34:58 -0700

    GUI-1596 small clean up of .modalBuilder

--- js/exportModal.js
+++ js/exportModal.js
@@ -46,12 +46,7 @@ window.ExportModal = (function($, ExportModal) {
             Tips.refresh();
         });
 
-        $exportModal.css({
-            "left"  : 0,
-            "right" : 0,
-            "top"   : 0,
-            "bottom": 0
-        });
+        centerPositionElement($exportModal);
 
         $(document).on("click.exportModal", function() {
             xcHelper.hideDropdowns($exportModal);
--- js/workbook.js
+++ js/workbook.js
@@ -42,12 +42,7 @@ window.WorkbookModal = (function($, WorkbookModal) {
             Tips.refresh();
         });
 
-        $workbookModal.css({
-            "left"  : 0,
-            "right" : 0,
-            "top"   : 0,
-            "bottom": 0
-        });
+        centerPositionElement($workbookModal);
         $workbookModal.show();
 
         resetWorkbookModal();
