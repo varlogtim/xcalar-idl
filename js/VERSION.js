a984b7a907a32b6add23021a25c2d8248fccde6f
Jerene Yang <jyang@xcalar.com>
2015-08-21 18:22:33 -0700

    GUI-1880 Version frontend KV

---
+++ js/VERSION.js
@@ -0,0 +1,7 @@
+var majorVersion = "0";
+var minorVersion = "9";
+var revisionVersion = "6";
+var thriftInterfaceVersion = "2";
+var kvVersion = "0";
+var fullVersion = majorVersion+"."+minorVersion+"."+revisionVersion+"."+
+                  thriftInterfaceVersion+"."+kvVersion;
