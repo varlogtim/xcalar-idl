e506fecbc91c46d5a89a63ac924cb7792a2089eb
Cheng Zhang <czhang@xcalar.com>
2015-10-13 12:26:07 -0700

    GUI-2153 Global change of css's initial property

--- js/script.js
+++ js/script.js
@@ -479,6 +479,7 @@ function documentReadyGeneralFunction() {
                     return true;
                 }
 
+                $(".colMenu").hide();
                 $rowInput.val(rowToGo).trigger(fakeEvent.enter);
 
                 return true;
