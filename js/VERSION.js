4fb814de30122b64c6b296322af886e1a5c7feff
Jerene Yang <jyang@xcalar.com>
2015-10-15 17:21:57 -0700

    GUI-2203 Bump up version

--- js/VERSION.js
+++ js/VERSION.js
@@ -1,7 +1,7 @@
 var majorVersion = "0";
 var minorVersion = "9";
-var revisionVersion = "6";
-var thriftInterfaceVersion = "2";
+var revisionVersion = "7";
+var thriftInterfaceVersion = "3";
 var kvVersion = "0";
 var fullVersion = majorVersion + "." + minorVersion + "." +
                     revisionVersion + "." +
