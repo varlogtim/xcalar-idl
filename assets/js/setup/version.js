09ae76931a7351f3f527d45823ddb263a4b306cf
Jerene Yang <jyang@xcalar.com>
2016-03-08 02:25:21 -0800

    GUI-3350 Update version

--- assets/js/setup/version.js
+++ assets/js/setup/version.js
@@ -3,7 +3,7 @@ window.XVM = (function(XVM) {
     var majorVersion = "0";
     var minorVersion = "9";
     var revisionVersion = "8";
-    var thriftInterfaceVersion = "6";
+    var thriftInterfaceVersion = "7";
     var kvVersion = "0";
     var fullVersion = majorVersion + "." + minorVersion + "." +
                         revisionVersion + "." +
