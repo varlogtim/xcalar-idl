4d2104616f3768ff04a47fdee83c5511529dac80
gagneron <rgagneron@xcalar.com>
2016-03-09 19:53:42 -0800

    GUI-3367 change structure of redo.js and undo.js

--- assets/js/undo.js
+++ assets/js/undo.js
@@ -24,373 +24,108 @@ window.Undo = (function($, Undo) {
         return (deferred.promise());
     };
 
+    var undoFuncs = {};
 
-    var undoFuncs = {
-        sort: function(options) {
+    undoFuncs[SQLOps.Filter] = function(options) {
+        var worksheet = WSManager.getWSFromTable(options.tableId);
+        return(TblManager.refreshTable([options.tableName], null,
+                                       [options.newTableName],
+                                       worksheet, {isUndo: true}));
+    };
 
-        },
+    undoFuncs[SQLOps.Map] = function(options) {
+        var worksheet = WSManager.getWSFromTable(options.tableId);
+        return(TblManager.refreshTable([options.tableName], null,
+                                       [options.newTableName],
+                                       worksheet, {isUndo: true}));
+    };
 
-        filter: function(options) {
-            var worksheet = WSManager.getWSFromTable(options.tableId);
-            return(TblManager.refreshTable([options.tableName], null,
-                                           [options.newTableName],
-                                           worksheet, {isUndo: true}));
-        },
+    undoFuncs[SQLOps.Join] = function(options) {
+        var deferred = jQuery.Deferred();
 
-        aggregate: function(options) {
+        var currTableId = xcHelper.getTableId(options.newTableName);
+        var currTableWorksheet = WSManager.getWSFromTable(currTableId);
 
-        },
+        var lTableWorksheet = WSManager.getWSFromTable(options.lTableId);
+        var rTableWorksheet = WSManager.getWSFromTable(options.rTableId);
 
-        map: function(options) {
-            var worksheet = WSManager.getWSFromTable(options.tableId);
-            return(TblManager.refreshTable([options.tableName], null,
-                                           [options.newTableName],
-                                           worksheet, {isUndo: true}));
-        },
+        var leftTable = {
+            name: options.lTableName,
+            id: options.lTableId,
+            position: options.lTablePos,
+            worksheet: lTableWorksheet
+        };
 
-        join: function(options) {
-            var deferred = jQuery.Deferred();
+        var rightTable = {
+            name: options.rTableName,
+            id: options.rTableId,
+            position: options.rTablePos,
+            worksheet: rTableWorksheet
+        };
 
-            var currTableId = xcHelper.getTableId(options.newTableName);
-            var currTableWorksheet = WSManager.getWSFromTable(currTableId);
+        var isSelfJoin = false;
+        if (leftTable.id === rightTable.id) {
+            isSelfJoin = true;
+        }
 
-            var lTableWorksheet = WSManager.getWSFromTable(options.lTableId);
-            var rTableWorksheet = WSManager.getWSFromTable(options.rTableId);
+        var firstTable = {};
+        var secondTable = {};
 
-            var leftTable = {
-                name: options.lTableName,
-                id: options.lTableId,
-                position: options.lTablePos,
-                worksheet: lTableWorksheet
-            };
-
-            var rightTable = {
-                name: options.rTableName,
-                id: options.rTableId,
-                position: options.rTablePos,
-                worksheet: rTableWorksheet
-            };
-
-            var isSelfJoin = false;
-            if (leftTable.id === rightTable.id) {
-                isSelfJoin = true;
-            }
-
-            var firstTable = {};
-            var secondTable = {};
-
-            if (!isSelfJoin) {
-                if (leftTable.worksheet === rightTable.worksheet) {
-                    if (leftTable.position > rightTable.position) {
-                        var temp = rightTable;
-                        rightTable = leftTable;
-                        leftTable = temp;
-                    }
+        if (!isSelfJoin) {
+            if (leftTable.worksheet === rightTable.worksheet) {
+                if (leftTable.position > rightTable.position) {
+                    var temp = rightTable;
+                    rightTable = leftTable;
+                    leftTable = temp;
                 }
             }
+        }
 
-            if (currTableWorksheet !== leftTable.worksheet  &&
-                currTableWorksheet !== rightTable.worksheet) {
-                firstTable = leftTable;
-                secondTable = rightTable;
-            } else if (currTableWorksheet === leftTable.worksheet) {
-                firstTable = leftTable;
-                firstTable.position = null; // we will rely on newTable's position
-                secondTable = rightTable;
-            } else if (!isSelfJoin && currTableWorksheet === rightTable.worksheet) {
-                firstTable = rightTable;
-                firstTable.position = null; // we will rely on newTable's position
-                secondTable = leftTable;
+        if (currTableWorksheet !== leftTable.worksheet  &&
+            currTableWorksheet !== rightTable.worksheet) {
+            firstTable = leftTable;
+            secondTable = rightTable;
+        } else if (currTableWorksheet === leftTable.worksheet) {
+            firstTable = leftTable;
+            firstTable.position = null; // we will rely on newTable's position
+            secondTable = rightTable;
+        } else if (!isSelfJoin && currTableWorksheet === rightTable.worksheet) {
+            firstTable = rightTable;
+            firstTable.position = null; // we will rely on newTable's position
+            secondTable = leftTable;
+        }
+
+
+        TblManager.refreshTable([firstTable.name], null,
+                                [options.newTableName],
+                                firstTable.worksheet,
+                                {isUndo: true,
+                                 position: firstTable.position})
+        .then(function() {
+            if (isSelfJoin) {
+                deferred.resolve();
+            } else {
+                TblManager.refreshTable([secondTable.name], null, [],
+                                         secondTable.worksheet,
+                                         {isUndo: true,
+                                          position: secondTable.position})
+                .then(deferred.resolve)
+                .fail(deferred.reject);
             }
-
-
-            TblManager.refreshTable([firstTable.name], null,
-                                    [options.newTableName],
-                                    firstTable.worksheet,
-                                    {isUndo: true,
-                                     position: firstTable.position})
-            .then(function() {
-                if (isSelfJoin) {
-                    deferred.resolve();
-                } else {
-                    TblManager.refreshTable([secondTable.name], null, [],
-                                             secondTable.worksheet,
-                                             {isUndo: true,
-                                              position: secondTable.position})
-                    .then(deferred.resolve)
-                    .fail(deferred.reject);
-                }
-            })
-            .fail(deferred.reject);
-
-            return (deferred.promise());
-        },
-
-        groupBy: function(options) {
-
-        },
-
-        renameTable: function(options) {
-
-        },
-
-        deleteTable: function(options) {
-
-        },
-
-        destroyDataSet: function(options) {
-
-        },
-
-        exportTable: function(options) {
-
-        },
-
-        addNewCol: function(options) {
-
-        },
-
-        deleteCol: function(options) {
-
-        },
-
-        hideCols: function(options) {
-            ColManager.unhideCols(options.colNums, options.tableId);
-            return (promiseWrapper(null));
-        },
-
-        unHideCols: function(options) {
-            ColManager.hideCols(options.colNums, options.tableId);
-            return (promiseWrapper(null));
-        },
-
-        duplicateCol: function(options) {
-
-        },
-
-        delDupCol: function(options) {
-
-        },
-
-        delAllDupCols: function(options) {
-
-        },
-
-        textAlign: function(options) {
-
-        },
-
-        reorderTable: function(options) {
-
-        },
-
-        reorderCol: function(options) {
-
-        },
-
-        renameCol: function(options) {
-
-        },
-
-        pullCol: function(options) {
-
-        },
-
-        archiveTable: function(options) {
-
-        },
-
-        // tableBulkActions: function(options) {
-        //     var action = options.action;
-        //     var tableType = options.tableType;
-        //     var tableName = options.tableName;
-        //     var tableId;
-
-        //     if (tableType === TableType.InActive) {
-        //         tableId = getTableId(xcHelper.getTableId(tableName));
-        //         $('#inactiveTablesList .tableInfo[data-id="' +
-        //                 tableId + '"] .addTableBtn').click();
-        //     } else if (tableType === TableType.Orphan) {
-        //         $('#orphanedTableList .tableInfo[data-tablename="' +
-        //                 tableName + '"] .addTableBtn').click();
-        //     } else {
-        //         console.error("Invalid table bulk action");
-        //         return (promiseWrapper(null));
-        //     }
-
-        //     return (TableList.tableBulkAction(action, tableType));
-        // },
-
-        sortTableCols: function(options) {
-
-        },
-
-        resizeTableCols: function(options) {
-
-        },
-
-        dragResizeTableCol: function(options, isReverse) {
-
-        },
-
-        hideTable: function(options) {
-
-        },
-
-        unhideTable: function(options) {
-
-        },
-
-        addWorksheet: function(options) {
-
-        },
-
-        renameWorksheet: function(options) {
-
-        },
-
-        switchWorksheet: function(options) {
-
-        },
-
-        reorderWorksheet: function(options) {
-
-        },
-
-        deleteWorksheet: function(options) {
-
-        },
-
-        moveTableToWorkSheet: function(options) {
-
-        },
-
-        // addNoSheetTables: function(options) {
-        //     var tableIds = options.tableIds;
-        //     for (var i = 0, len = tableIds.length; i < len; i++) {
-        //         tableIds[i] = getTableId(tableIds[i]);
-        //     }
-
-        //     var wsIndex = options.worksheetIndex;
-        //     var wsId    = WSManager.getOrders()[wsIndex];
-
-        //     WSManager.addNoSheetTables(tableIds, wsId);
-        //     return (promiseWrapper(null));
-        // },
-
-        moveInactiveTableToWorksheet: function(options) {
-
-        },
-
-        createFolder: function(options) {
-
-        },
-
-        dsRename: function(options) {
-
-        },
-
-        profile: function(options, keepOpen) {
-
-        },
-
-        profileSort: function(options, keepOpen) {
-
-        },
-
-        profileBucketing: function(options, keepOpen) {
-
-        },
-
-        quickAgg: function(options) {
-
-        },
-
-        correlation: function(options) {
-
-        },
-
-        addOtherUserDS: function(options) {
-
-        },
-
-        splitCol: function(options) {
-
-        },
-
-        changeType: function(options) {
-
-        },
-
-        changeFormat: function(options) {
-
-        },
-
-        roundToFixed: function(options) {
-
-        },
-
-        // correlationAction: function(options) {
-
-        // },
-
-        // groupByAction: function(options) {
-
-        // },
-
-        // renameOrphanTable: function(options) {
-
-        // },
-
-        // addDataset: function(options) {
-
-        // },
-        // horizontalPartitionAction: function(options) {
-
-        // },
-
-        // changeTypeMap: function(options) {
-
-        // },
-
-        // profileAction: function(options) {
-
-        // },
-        // spltColMap: function(options) {
-
-        // },
-
-        // previewDataSet: function(options) {
-
-        // },
-
-        // multiJoinMap: function(options) {
-
-        // },
-
-        // window: function(options) {
-
-        // },
-
-        // horizontalPartition: function(options) {
-
-        // },
-
-        // destroyPreviewDataSet: function(options) {
-
-        // },
-
-        // quickAggAction: function(options) {
-
-        // },
-
-        // checkIndex: function(options) {
-
-        // },
-
-        // windowAction: function(options) {
-
-        // },
+        })
+        .fail(deferred.reject);
+
+        return (deferred.promise());
+    };
+
+    undoFuncs[SQLOps.HideCols] = function(options) {
+        ColManager.unhideCols(options.colNums, options.tableId);
+        return (promiseWrapper(null));
+    };
+
+    undoFuncs[SQLOps.UnHideCols] = function(options) {
+        ColManager.hideCols(options.colNums, options.tableId);
+        return (promiseWrapper(null));
     };
 
     return (Undo);
