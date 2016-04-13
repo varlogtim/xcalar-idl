32f9ff48f139c9edaaaf4d618977564e36be2918
Cheng Zhang <czhang@xcalar.com>
2016-04-13 13:34:20 -0700

    GUI-3646 Wrong path of tableau visualize
    Change-Id: I2eae2f8a8f91c0d0b24741e71ce31d1c46e11d49

--- assets/lang/en/htmlTStr.js
+++ assets/lang/en/htmlTStr.js
@@ -74,6 +74,26 @@ module.exports = {
         'PleaseWait': 'Please Wait'
     },
 
+    paths: {
+        'dologout'      : 'assets/htmlFiles/dologout.html',
+        'login'         : 'assets/htmlFiles/login.html',
+        'tableau'       : 'assets/htmlFiles/tableau.html',
+        'indexAbsolute' : "/index.html",
+        'index'         : 'index.html',
+        'testAbsolute'  : '/unitTest.html',
+        'workbookDemo'  : 'assets/htmlFiles/walk/workbookDemo.html',
+        'datastoreDemo1': 'assets/htmlFiles/walk/datastoreDemo1.html',
+        'datastoreDemo2': 'assets/htmlFiles/walk/datastoreDemo2.html',
+        'monitor'       : 'assets/htmlFiles/monitor.html',
+        'dagBackground' : 'assets/images/dag-background.png',
+        'dbDiamond'     : 'assets/images/dbDiamond.png',
+        'dTable'        : 'assets/images/dtable.png',
+        'roundedRect'   : 'assets/images/rounded-rect.png',
+        'hourglass'     : 'assets/images/hourglass-white.png',
+        'waitIcon'      : 'assets/images/waiting-1.gif',
+        'installer'     : 'setup.html'
+    },
+
     menuBar: {
         'ws': 'Worksheet',
         'monitor': 'Monitor',
