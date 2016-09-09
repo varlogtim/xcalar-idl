5ce7e54fe57629a47b17700c50fad6202a9283cc
Cheng Zhang <czhang@xcalar.com>
2016-09-08 17:57:32 -0700

    GUI-5354 Add Array.includes to compatible.js
    Change-Id: I603667d6b76845d76703fd01befcfde46405ed45

--- assets/js/setup/compatible.js
+++ assets/js/setup/compatible.js
@@ -67,91 +67,130 @@ window.Compatible = (function($, Compatible) {
 
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
         if (!Array.prototype.map) {
+            Object.defineProperty(Array.prototype, "map", {
+               enumerable: false,
+               writable: true,
+               value: function(callback, thisArg) {
+                   var T, A, k;
 
-          Array.prototype.map = function(callback, thisArg) {
+                    if (this == null) {
+                      throw new TypeError(' this is null or not defined');
+                    }
 
-            var T, A, k;
+                    // 1. Let O be the result of calling ToObject passing the |this| 
+                    //    value as the argument.
+                    var O = Object(this);
 
-            if (this == null) {
-              throw new TypeError(' this is null or not defined');
-            }
+                    // 2. Let lenValue be the result of calling the Get internal 
+                    //    method of O with the argument "length".
+                    // 3. Let len be ToUint32(lenValue).
+                    var len = O.length >>> 0;
 
-            // 1. Let O be the result of calling ToObject passing the |this| 
-            //    value as the argument.
-            var O = Object(this);
+                    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+                    // See: http://es5.github.com/#x9.11
+                    if (typeof callback !== 'function') {
+                      throw new TypeError(callback + ' is not a function');
+                    }
 
-            // 2. Let lenValue be the result of calling the Get internal 
-            //    method of O with the argument "length".
-            // 3. Let len be ToUint32(lenValue).
-            var len = O.length >>> 0;
+                    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                    if (arguments.length > 1) {
+                      T = thisArg;
+                    }
 
-            // 4. If IsCallable(callback) is false, throw a TypeError exception.
-            // See: http://es5.github.com/#x9.11
-            if (typeof callback !== 'function') {
-              throw new TypeError(callback + ' is not a function');
-            }
+                    // 6. Let A be a new array created as if by the expression new Array(len) 
+                    //    where Array is the standard built-in constructor with that name and 
+                    //    len is the value of len.
+                    A = new Array(len);
 
-            // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-            if (arguments.length > 1) {
-              T = thisArg;
-            }
+                    // 7. Let k be 0
+                    k = 0;
 
-            // 6. Let A be a new array created as if by the expression new Array(len) 
-            //    where Array is the standard built-in constructor with that name and 
-            //    len is the value of len.
-            A = new Array(len);
+                    // 8. Repeat, while k < len
+                    while (k < len) {
 
-            // 7. Let k be 0
-            k = 0;
+                      var kValue, mappedValue;
 
-            // 8. Repeat, while k < len
-            while (k < len) {
+                      // a. Let Pk be ToString(k).
+                      //   This is implicit for LHS operands of the in operator
+                      // b. Let kPresent be the result of calling the HasProperty internal 
+                      //    method of O with argument Pk.
+                      //   This step can be combined with c
+                      // c. If kPresent is true, then
+                      if (k in O) {
 
-              var kValue, mappedValue;
+                        // i. Let kValue be the result of calling the Get internal 
+                        //    method of O with argument Pk.
+                        kValue = O[k];
 
-              // a. Let Pk be ToString(k).
-              //   This is implicit for LHS operands of the in operator
-              // b. Let kPresent be the result of calling the HasProperty internal 
-              //    method of O with argument Pk.
-              //   This step can be combined with c
-              // c. If kPresent is true, then
-              if (k in O) {
+                        // ii. Let mappedValue be the result of calling the Call internal 
+                        //     method of callback with T as the this value and argument 
+                        //     list containing kValue, k, and O.
+                        mappedValue = callback.call(T, kValue, k, O);
 
-                // i. Let kValue be the result of calling the Get internal 
-                //    method of O with argument Pk.
-                kValue = O[k];
+                        // iii. Call the DefineOwnProperty internal method of A with arguments
+                        // Pk, Property Descriptor
+                        // { Value: mappedValue,
+                        //   Writable: true,
+                        //   Enumerable: true,
+                        //   Configurable: true },
+                        // and false.
 
-                // ii. Let mappedValue be the result of calling the Call internal 
-                //     method of callback with T as the this value and argument 
-                //     list containing kValue, k, and O.
-                mappedValue = callback.call(T, kValue, k, O);
+                        // In browsers that support Object.defineProperty, use the following:
+                        // Object.defineProperty(A, k, {
+                        //   value: mappedValue,
+                        //   writable: true,
+                        //   enumerable: true,
+                        //   configurable: true
+                        // });
 
-                // iii. Call the DefineOwnProperty internal method of A with arguments
-                // Pk, Property Descriptor
-                // { Value: mappedValue,
-                //   Writable: true,
-                //   Enumerable: true,
-                //   Configurable: true },
-                // and false.
+                        // For best browser support, use the following:
+                        A[k] = mappedValue;
+                      }
+                      // d. Increase k by 1.
+                      k++;
+                    }
 
-                // In browsers that support Object.defineProperty, use the following:
-                // Object.defineProperty(A, k, {
-                //   value: mappedValue,
-                //   writable: true,
-                //   enumerable: true,
-                //   configurable: true
-                // });
+                    // 9. return A
+                    return A;
+               }
+            });
+        }
 
-                // For best browser support, use the following:
-                A[k] = mappedValue;
-              }
-              // d. Increase k by 1.
-              k++;
-            }
+        if (!Array.prototype.includes) {
+            Object.defineProperty(Array.prototype, "includes", {
+               enumerable: false,
+               writable: true,
+               value: function(searchElement) {
+                   'use strict';
+                    if (this == null) {
+                      throw new TypeError('Array.prototype.includes called on null or undefined');
+                    }
 
-            // 9. return A
-            return A;
-          };
+                    var O = Object(this);
+                    var len = parseInt(O.length, 10) || 0;
+                    if (len === 0) {
+                      return false;
+                    }
+                    var n = parseInt(arguments[1], 10) || 0;
+                    var k;
+                    if (n >= 0) {
+                      k = n;
+                    } else {
+                      k = len + n;
+                      if (k < 0) {k = 0;}
+                    }
+                    var currentElement;
+                    while (k < len) {
+                      currentElement = O[k];
+                      if (searchElement === currentElement ||
+                         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+                        return true;
+                      }
+                      k++;
+                    }
+                    return false;
+               }
+            });
         }
     }
 
