78a943e9bc4101aeef0b6dccfad0e4c55d9e0a99
Jerene Yang <jyang@xcalar.com>
2016-11-01 15:46:49 -0700

    GUI-6044 Fixes to expServerSync script
    Change-Id: I0fceade96bb625613bed81afffc5f4e7b57cf8de

--- assets/bin/expServerSync.sh
+++ assets/bin/expServerSync.sh
@@ -1,18 +1,19 @@
 scp ~/xcalar-gui/services/expServer/expServer.js boya@dijkstra:~/xcalar-gui/services/expServer
+scp ~/xcalar-gui/services/expServer/expServer.js boya@bellman:~/xcalar-gui/services/expServer
 
 ssh boya@dijkstra '
 set +e
 set -x
 PID=`ps aux | grep -v bash | grep -v grep | grep expServer | tr -s " " | cut -d " " -f 2`
-kill -9 ${PID}
+sudo kill -9 ${PID}
 cd ~/xcalar-gui/services/expServer
-nodejs expServer.js'
+nodejs expServer.js' &
 
 ssh boya@bellman '
 set +e
 set -x
 PID=`ps aux | grep -v bash | grep -v grep | grep expServer | tr -s " " | cut -d " " -f 2`
-kill -9 ${PID}
+sudo kill -9 ${PID}
 cd ~/xcalar-gui/services/expServer
-nodejs expServer.js'
+nodejs expServer.js' &
 
