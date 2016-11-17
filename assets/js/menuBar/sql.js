f730cd5232ccabdd61c7b23be524c45d737a31db
Cheng Zhang <czhang@xcalar.com>
2016-11-16 19:37:19 -0800

    GUI-6287 Unit test for sql.js
    Change-Id: I0dd32332e0d81cf78222ed31cd8b0cf1e53ac3a4

--- assets/js/menuBar/sql.js
+++ assets/js/menuBar/sql.js
@@ -108,11 +108,9 @@ window.SQL = (function($, SQL) {
             return deferred.resolve().promise();
         }
 
-        var newErrors = errors;
-
         restoreLogs(oldLogCursor)
         .then(function() {
-            return restoreErrors(newErrors);
+            return restoreErrors();
         })
         .then(function() {
             // XXX FIXME change back to localCommit() if it's buggy
@@ -512,7 +510,6 @@ window.SQL = (function($, SQL) {
                 console.error("Loose old cursor track");
                 oldLogCursor = oldLogs.length - 1;
             }
-
             for (var i = 0; i <= oldLogCursor; i++) {
                 var sql = new XcLog(oldLogs[i]);
                 addLog(sql, true);
@@ -526,7 +523,7 @@ window.SQL = (function($, SQL) {
         return (deferred.promise());
     }
 
-    function restoreErrors(newErrors) {
+    function restoreErrors() {
         var deferred = jQuery.Deferred();
         var oldErrors = [];
 
@@ -552,12 +549,14 @@ window.SQL = (function($, SQL) {
             }
         })
         .then(function() {
+            if (errors.length > 0) {
+                console.warn(errors);
+            }
+
             oldErrors.forEach(function(oldErr) {
                 var errorLog = new XcLog(oldErr);
                 errors.push(errorLog);
             });
-
-            errors = errors.concat(newErrors);
 
             deferred.resolve();
         })
@@ -1000,185 +999,16 @@ window.SQL = (function($, SQL) {
         }
     }
 
-    // function cliRenameColHelper(options) {
-    //     // rename <dataset> <existingColName> <newColName>
-    //     var string = "rename";
-    //     string += " " + options.dsName;
-    //     string += " " + options.oldColName;
-    //     string += " " + options.newColName;
-    //     return (string);
-    // }
-
-    // function cliRetypeColHelper(options) {
-    //     // cast <dataset> <existingColName> <newColType>
-    //     var string = "cast";
-    //     string += " " + options.dsName;
-    //     string += " " + options.colName;
-
-    //     switch (options.newType) {
-    //         case ("string"):
-    //             string += " DfString";
-    //             break;
-    //         case ("integer"):
-    //             string += " DfInt64";
-    //             break;
-    //         case ("float"):
-    //             string += " DfFloat64";
-    //             break;
-    //         case ("boolean"):
-    //             string += " DfBoolean";
-    //             break;
-    //         case ("mixed"):
-    //             string += " DfMixed";
-    //             break;
-    //         case ("undefined"):
-    //             // fallthrough
-    //         default:
-    //             string += " DfUnknown";
-    //             break;
-    //     }
-    //     return (string);
-    // }
-
-    // function cliLoadHelper(options) {
-    //     // load --url <url> --format <format> --name <dsName>
-    //     var string = "load";
-    //     string += " --url";
-    //     string += " " + options.dsPath;
-    //     string += " --format";
-    //     string += " " + options.dsFormat.toLowerCase();
-    //     string += " --name";
-    //     string += " " + options.dsName;
-    //     if (options.fieldDelim && options.fieldDelim !== "Null") {
-    //         var fd = JSON.stringify(options.fieldDelim);
-    //         if (fd.indexOf("\\") !== 1) {
-    //             string += " --fielddelim";
-    //             string += " " + fd;
-    //         }
-    //     }
-    //     if (options.lineDelim && options.lineDelim !== "Null") {
-    //         var rd = JSON.stringify(options.lineDelim);
-    //         if (rd.indexOf("\\") !== 1) {
-    //             string += " --recorddelim";
-    //             string += " " + rd;
-    //         }
-    //     }
-    //     return (string);
-    // }
-
-    // function cliDeleteHelper(options) {
-    //     // drop <dropWhat> <name>
-    //     var string    = "drop";
-    //     var operation = options.operation;
-
-    //     if (operation === "destroyDataSet") {
-    //         string += " dataset";
-    //         string += " " + options.dsName;
-    //     } else if (operation === "deleteTable") {
-    //         string += " table";
-    //         string += " " + options.tableName;
-    //     }
-    //     return (string);
-    // }
-
-    // function cliFilterHelper(options) {
-    //     // filter <tableName> <"filterStr"> <filterTableName>
-    //     var string = "filter";
-    //     var flt    = options.filterString;
-
-    //     string += " " + options.tableName;
-    //     if (!flt) {
-    //         flt = generateFilterString(options.operator,
-    //                                    options.backColName,
-    //                                    options.value);
-    //     }
-    //     // Now we need to escape quotes. We don't need to do it for the thrift
-    //     // call because that's a thrift field. However, now that everything is
-    //     // lumped into one string, we have to do some fun escaping
-
-    //     flt = flt.replace(/["']/g, "\\$&");
-    //     string += " \"" + flt + "\"";
-    //     string += " " + options.newTableName;
-    //     return (string);
-    // }
-
-    // function cliIndexHelper(options) {
-    //     // index --key <keyname> --dataset <dataset> | --srctable <tableName>
-    //     // --dsttable <tableName>
-    //     var string = "index";
-    //     string += " --key";
-    //     string += " " + options.key;
-    //     if (options.operation === "sort") {
-    //         string += " --srctable";
-    //         string += " " + options.tableName;
-    //     } else if (options.operation === "index") {
-    //         string += " --dataset";
-    //         string += " " + options.dsName;
-    //     }
-    //     string += " --dsttable";
-    //     string += " " + options.newTableName;
-    //     return (string);
-    // }
-
-    // function cliJoinHelper(options) {
-    //     // join --leftTable <leftTable> --rightTable <rightTable>
-    //     // --joinTable <joinTable> --joinType <joinType>
-    //     var string = "join";
-    //     string += " --leftTable";
-    //     string += " " + options.leftTable.name;
-    //     string += " --rightTable";
-    //     string += " " + options.rightTable.name;
-    //     string += " --joinTable";
-    //     string += " " + options.newTableName;
-    //     string += " --joinType";
-    //     var joinType = options.joinType.replace(" ", "");
-    //     joinType = joinType.charAt(0).toLowerCase() + joinType.slice(1);
-    //     string += " " + joinType;
-    //     return (string);
-    // }
-
-    // function cliGroupByHelper(options) {
-    //     // groupBy <tableName> <operator> <fieldName> <newFieldName>
-    //     // <groupByTableName>
-    //     var string = "groupBy";
-    //     string += " " + options.backname;
-    //     switch (options.operator) {
-    //         case ("Average"):
-    //             string += " " + "avg";
-    //             break;
-    //         case ("Count"):
-    //             string += " " + "count";
-    //             break;
-    //         case ("Sum"):
-    //             string += " " + "sum";
-    //             break;
-    //         case ("Max"):
-    //             string += " " + "max";
-    //             break;
-    //         case ("Min"):
-    //             string += " " + "min";
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    //     string += " " + options.backFieldName;
-    //     string += " " + options.newColumnName;
-    //     string += " " + options.newTableName;
-    //     return (string);
-    // }
-
-    // function cliMapHelper(options) {
-    //     var string = "map";
-    //     string += " --eval";
-    //     string += " \"map(" + options.mapString + ")\"";
-    //     string += " --srctable";
-    //     string += " " + options.backname;
-    //     string += " --fieldName";
-    //     string += " " + options.colName;
-    //     string += " --dsttable";
-    //     string += " " + options.newTableName;
-    //     return (string);
-    // }
+    /* Unit Test Only */
+    if (window.unitTestMode) {
+        SQL.__testOnly__ = {};
+        SQL.__testOnly__.isBackendOperation = isBackendOperation;
+        SQL.__testOnly__.getCliMachine = getCliMachine;
+        SQL.__testOnly__.getCliHTML = getCliHTML;
+        SQL.__testOnly__.getUndoType = getUndoType;
+        SQL.__testOnly__.UndoType = UndoType;
+    }
+    /* End Of Unit Test Only */
 
     return (SQL);
 }(jQuery, {}));
---
+++ assets/test/unitTest/menuBar/sqlSpec.js
@@ -0,0 +1,437 @@
+describe("SQL Test", function() {
+    describe("Basic Function Test", function() {
+        it("isBackendOperation should work", function() {
+            var isBackendOperation = SQL.__testOnly__.isBackendOperation;
+
+            var testCases = [{
+                "operation": SQLOps.DupCol,
+                "expect"   : false
+            },{
+                "operation": SQLOps.DestroyDS,
+                "expect"   : true
+            },{
+                "operation": "Invalid operation",
+                "expect"   : null
+            }];
+
+            testCases.forEach(function(test) {
+                var args = {
+                    "options": {
+                        "operation": test.operation
+                    }
+                };
+                var sql = new XcLog(args);
+                var res = isBackendOperation(sql);
+                expect(res).to.equal(test.expect);
+            });
+        });
+
+        it("getUndoType should work", function() {
+            var getUndoType = SQL.__testOnly__.getUndoType;
+            var UndoType = SQL.__testOnly__.UndoType;
+            var testCases = [{
+                "operation": null,
+                "expect"   : UndoType.Invalid
+            },{
+                "operation": SQLOps.DSPoint,
+                "expect"   : UndoType.Invalid
+            },{
+                "operation": SQLOps.PreviewDS,
+                "expect"   : UndoType.Skip
+            },{
+                "operation": SQLOps.DupCol,
+                "expect"   : UndoType.Valid
+            }];
+
+            testCases.forEach(function(test) {
+                var args = {
+                    "options": {
+                        "operation": test.operation
+                    }
+                };
+                var sql = new XcLog(args);
+                var res = getUndoType(sql);
+                expect(res).to.equal(test.expect);
+            });
+        });
+
+        it("getCliMachine should work", function() {
+            var getCliMachine = SQL.__testOnly__.getCliMachine;
+
+            var testCases = [{
+                "operation": SQLOps.DupCol,
+                "error"    : "test error",
+                "cli"      : "test",
+                "expect"   : ""
+            },{
+                "operation": SQLOps.DupCol,
+                "cli"      : "test",
+                "expect"   : ""
+            },{
+                "operation": SQLOps.DestroyDS,
+                "cli"      : "test",
+                "expect"   : '<span class="cliWrap" data-cli=1>test</span>'
+            }];
+
+            testCases.forEach(function(test) {
+                var args = {
+                    "error"  : test.error,
+                    "cli"    : test.cli,
+                    "options": {
+                        "operation": test.operation
+                    }
+                };
+                var sql = new XcLog(args);
+                var res = getCliMachine(sql, 1);
+                expect(res).to.equal(test.expect);
+            });
+        });
+
+        it("getCliHTML should work", function() {
+            var getCliHTML = SQL.__testOnly__.getCliHTML;
+
+            var testCases = [{
+                "operation": SQLOps.DupCol,
+                "error"    : "test error",
+                "isValid"  : false
+            },{
+                "operation": SQLOps.PreviewDS, // cannot undoable
+                "isValid"  : false
+            },{
+                "operation": SQLOps.DupCol,
+                "title"    : "test",
+                "key"      : "testKey",
+                "isValid"  : true
+            }];
+
+            testCases.forEach(function(test) {
+                var args = {
+                    "title"  : test.title,
+                    "error"  : test.error,
+                    "options": {
+                        "operation": test.operation,
+                        "key"      : test.key
+                    }
+                };
+                var sql = new XcLog(args);
+                var res = getCliHTML(sql, 1);
+                if (test.isValid) {
+                    expect(res).not.to.equal("");
+                    var $sql = $('<div>' + res + '</div>');
+                    expect($sql.find(".sqlContentWrap").data("sql"))
+                    .to.equal(1);
+                    expect($sql.find(".key").length).to.equal(1);
+                } else {
+                    expect(res).to.equal("");
+                }
+            });
+        });
+    });
+
+    describe("Public API Test", function() {
+        it("SQL.hasUnCommitChange should work", function() {
+            var res = SQL.hasUnCommitChange();
+            expect(typeof res).to.equal("boolean");
+        });
+
+        it("SQL.getCursor should work", function() {
+            var res = SQL.getCursor();
+            expect(typeof res).to.equal("number");
+        });
+
+        it("SQL.getLogs should work", function() {
+            var res = SQL.getLogs();
+            expect(res instanceof Array).to.be.true;
+        });
+
+        it("SQL.getErrorLogs should work", function() {
+            var res = SQL.getErrorLogs();
+            expect(res instanceof Array).to.be.true;
+        });
+
+        it("SQL.getAllLogs should work", function() {
+            var res = SQL.getAllLogs();
+            expect(res).to.an("object");
+            expect(res.logs).to.equal(SQL.getLogs());
+            expect(res.errors).to.equal(SQL.getErrorLogs());
+        });
+
+        it("SQL.getLocalStorage should work", function() {
+            var cachedSQL = SQL.getLocalStorage();
+            expect(cachedSQL == null || typeof cachedSQL === "string");
+        });
+
+        it("should backup sql", function() {
+            SQL.backup();
+            var cachedSQL = SQL.getBackup();
+            expect(cachedSQL).not.to.be.null;
+            expect(typeof cachedSQL === "string");
+        });
+
+        it("SQL.viewLastAction should work", function() {
+            var res = SQL.viewLastAction();
+            var res2 = SQL.viewLastAction(true);
+            expect(res).not.to.equal(res2);
+        });
+    });
+
+    describe("Clean, Add, Restore SQL Test", function() {
+        before(function() {
+            // should not have any auto commit during test
+            Support.stopHeartbeatCheck();
+        });
+
+        it("SQL.add should work", function() {
+            var logs = SQL.getLogs();
+            var len = logs.length;
+            // error case
+            SQL.add("test", {}, "testCli", true);
+            expect(logs.length - len).to.equal(0);
+
+            SQL.add("test", {"operation": SQLOps.DupCol}, "testCli", true);
+            expect(logs.length - len).to.equal(1);
+        });
+
+        it("SQL.errorLog should work", function() {
+            var errors = SQL.getErrorLogs();
+            var len = errors.length;
+
+            SQL.errorLog("test", null, "testCli", "testError");
+            expect(errors.length - len).to.equal(1);
+        });
+
+        it("Should clear sql", function() {
+            SQL.clear();
+            expect(SQL.getLogs().length).to.equal(0);
+        });
+
+        it("Should restore sql in empty state", function(done) {
+            var logs = SQL.getLogs();
+            var len = logs.length;
+
+            SQL.restore(null, true)
+            .then(function() {
+                expect(logs.length).to.equal(len);
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            });
+        });
+
+        it("Should not restore in error case", function(done) {
+            var oldFunc = KVStore.get;
+            KVStore.get = function() {
+                return PromiseHelper.reject({"error": "test"});
+            };
+
+            SQL.restore()
+            .then(function() {
+                throw "error case";
+            })
+            .fail(function(error) {
+                expect(error).not.to.be.null;
+                expect(error.error).to.equal("test");
+                done();
+            })
+            .always(function() {
+                KVStore.get = oldFunc;
+            });
+        });
+
+        it("Should restore old sql", function(done) {
+            var logs = SQL.getLogs();
+            var len = logs.length;
+
+            SQL.restore()
+            .then(function() {
+                if (len !== 0) {
+                    expect(logs.length).not.to.equal(len);
+                }
+                
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            });
+        });
+
+        after(function() {
+            Support.heartbeatCheck();
+        });
+    });
+
+    describe("Undo Redo Api Test", function() {
+        var oldUndo;
+        var oldRedo;
+
+        before(function() {
+            oldUndo = Undo.run;
+            oldRedo = Redo.run;
+
+            Undo.run = function() {
+                return PromiseHelper.resolve();
+            };
+
+            Redo.run = function() {
+                return PromiseHelper.resolve();
+            };
+
+            Support.stopHeartbeatCheck();
+            SQL.add("test", {"operation": SQLOps.DupCol}, "testCli", true);
+        });
+
+        it("SQL.isUndo should work", function() {
+            var res = SQL.isUndo();
+            expect(typeof res).to.equal("boolean");
+        });
+
+        it("SQL.isRedo should work", function() {
+            var res = SQL.isRedo();
+            expect(typeof res).to.equal("boolean");
+        });
+
+        it("Should lock and unlock undo redo", function() {
+            var $undo = $("#undo");
+            var $redo = $("#redo");
+            SQL.lockUndoRedo();
+            expect($undo.hasClass("disabled")).to.be.true;
+            SQL.unlockUndoRedo();
+            expect($redo.hasClass("disabled")).to.be.true;
+        });
+
+        it("Should not undo in error case", function(done) {
+            Undo.run = function() {
+                return PromiseHelper.reject({"error": "test"});
+            };
+
+            SQL.undo()
+            .then(function() {
+                throw "error case";
+            })
+            .fail(function(error) {
+                expect(error).not.to.be.null;
+                expect(error.error).to.equal("test");
+                done();
+            });
+        });
+
+        it("Should undo in normal case", function(done) {
+            Undo.run = function() {
+                return PromiseHelper.resolve();
+            };
+
+            var cursor = SQL.getCursor();
+            SQL.undo()
+            .then(function() {
+                expect(SQL.getCursor()).to.equal(cursor - 1);
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            });
+        });
+
+        it("Should not redo in error case", function(done) {
+            Redo.run = function() {
+                return PromiseHelper.reject({"error": "test"});
+            };
+
+            SQL.redo()
+            .then(function() {
+                throw "error case";
+            })
+            .fail(function(error) {
+                expect(error).not.to.be.null;
+                expect(error.error).to.equal("test");
+                done();
+            });
+        });
+
+        it("Should redo in normal case", function(done) {
+            Redo.run = function() {
+                return PromiseHelper.resolve();
+            };
+
+            var cursor = SQL.getCursor();
+            SQL.redo()
+            .then(function() {
+                expect(SQL.getCursor()).to.equal(cursor + 1);
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            });
+        });
+
+        after(function(done) {
+            xcTooltip.hideAll();
+            SQL.clear();
+            SQL.restore()
+            .then(function() {
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            })
+            .always(function() {
+                Undo.run = oldUndo;
+                Redo.run = oldRedo;
+                Support.heartbeatCheck();
+            });
+        });
+    });
+
+    describe("SQl Menu Behavior Test", function() {
+        var $sqlButtons;
+        var $textarea;
+        var $machineTextarea;
+
+        before(function() {
+            $sqlButtons = $("#sqlButtonWrap");
+            $textarea = $("#sql-TextArea");
+            $machineTextarea = $("#sql-MachineTextArea");
+        });
+
+        it("Should view machine SQL", function() {
+            $sqlButtons.find(".humanLog").click();
+            expect($machineTextarea.get(0).style.display)
+            .to.equal("block");
+            expect($textarea.get(0).style.display)
+            .to.equal("none");
+        });
+
+        it("Should view human SQL", function() {
+            $sqlButtons.find(".machineLog").click();
+            expect($machineTextarea.get(0).style.display)
+            .to.equal("none");
+            expect($textarea.get(0).style.display)
+            .to.equal("block");
+        });
+
+        it("Should copy log", function(done) {
+            var oldFunc = xcHelper.assert;
+            // it will fail the test 
+            xcHelper.assert = function() { return true };
+
+            $sqlButtons.find(".copyLog").click();
+            var $successMessageWrap = $("#successMessageWrap");
+            assert.isTrue($successMessageWrap.is(":visible"));
+
+            var checkFunc = function() {
+                return !$successMessageWrap.is(":visible");
+            };
+
+            UnitTest.testFinish(checkFunc)
+            .then(function() {
+                done();
+            })
+            .fail(function() {
+                throw "error case";
+            })
+            .always(function() {
+                xcHelper.assert = oldFunc;
+            });
+        });
+    });
+});
