0e45bbc9679e3f9bbe4572bcc2628e31e2779592
Jerene Yang <jyang@xcalar.com>
2017-04-02 23:36:51 -0700

    GUI-7818 Just kidding
    Change-Id: Ic5444d32ab3230a2fa43323196c1c5f8a02d1790

--- assets/bin/cantorUpdateThrift.sh
+++ assets/bin/cantorUpdateThrift.sh
@@ -2,6 +2,7 @@ cd ~/gui/assets/js/thrift/
 mv thrift.js 015
 #cp $XLRDIR/src/bin/thrift/js/*.js .
 #cp $XLRDIR/src/bin/tests/*.js .
+rm ~/gui/assets/js/thrift/*
 scp jyang@cantor:~/xcalar/src/bin/thrift/js/*.js .
 scp jyang@cantor:~/xcalar/src/bin/tests/*.js .
 cp 015/thrift.js .
