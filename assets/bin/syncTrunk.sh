55b85b4d2a6fe5766f78afe1bb99fb39e5fa12cc
Jerene Yang <jyang@xcalar.com>
2017-12-07 10:57:20 -0800

    GUI-10489 Fix make trunk
    Change-Id: I8c68135e0fd3b9e0ce9e0d747ee29e01bd4b194d

--- assets/bin/syncTrunk.sh
+++ assets/bin/syncTrunk.sh
@@ -3,9 +3,7 @@ set -e
 
 # Check if envvar PRODUCTNAME is set. If it is, use that. Else use xcalar-gui
 cd $XLRGUIDIR/xcalar-gui/assets/js/thrift/
-mv *.js 015
 cp $XLRDIR/bin/jsPackage/*.js .
-cp 015/thrift.js .
 
 cd $XLRGUIDIR
 ln -s xcalar-gui prod
