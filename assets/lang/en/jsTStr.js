25a02e41ed0567c8594ca7b7de60fc0dea4701c1
Rudy Gagneron <rgagneron@xcalar.com>
2018-03-08 19:56:18 -0800

    GUI-11276 wrong error message for module udf errors
    Change-Id: Id774e3560bd9328e8f0128012d31eae8b4c3a10b

--- assets/lang/en/jsTStr.js
+++ assets/lang/en/jsTStr.js
@@ -1161,7 +1161,7 @@ UDFTStr = {
     // Must be kept consistent with htmlTStr.UDF
     "NameHint": "Module name (lowercase)",
     "NoTemplate": "Cannot find the module",
-    "InValidName": "Module name should start with a letter or underscore and contain only letters, digits, hyphens(-) or underscores(_)",
+    "InValidName": "Module name should start with a lowercase letter or underscore and contain only letters, digits, hyphens(-) or underscores(_)",
     "InValidFnName": "Function name should start with a lowercase letter or underscore and contain only letters, digits or underscores(_)"
 };
 
