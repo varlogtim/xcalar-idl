12d6ec75cd7371c74d06e339d18c53640cab8691
Cheng Zhang <czhang@xcalar.com>
2018-04-06 16:39:12 -0700

    GUI-11616 Rewrite csHelp to ts
    Change-Id: I76df61a574466a7f802fcd5f927d0b337dd651d1

---
+++ ts/shared/helperClasses/csHelp.ts
@@ -0,0 +1,18 @@
+class CSHelp {
+    private static _instance: CSHelp;
+
+    private constructor() {
+        const lookup = csLookup;
+        const helpBaseUrl = paths.helpUserContent;
+
+        $(document).on("click", ".csHelp", function() {
+            const topic = $(this).attr("data-topic");
+            const url = helpBaseUrl + lookup[topic];
+            window.open(url, "xcalar");
+        });
+    }
+
+    public static setup(): CSHelp {
+        return this._instance || (this._instance = new this());
+    }
+}
