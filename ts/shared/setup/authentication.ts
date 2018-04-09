4b404fe383aea376dbcdf7856d8d84ea380b09aa
Daniel Kaper <dkaper-intern@xcalar.com>
2018-04-09 00:58:59 -0400

    GUI-11620 converted authentication.js to typescript
    Change-Id: Ibf81a262ad5dc22e1fb858c82b99bccf144c5c98

---
+++ ts/shared/setup/authentication.ts
@@ -0,0 +1,54 @@
+class Authentication {
+    private static authInfo: XcAuth;
+    private static kvStore: KVStore;
+
+    /**
+     * Authentication.setup
+     */
+    public static setup(): XDPromise<void> {
+        const deferred: XDDeferred<void> = PromiseHelper.deferred();
+        const key: string = KVStore.getKey("gAuthKey");
+        Authentication.kvStore = new KVStore(key, gKVScope.AUTH);
+
+        Authentication.kvStore.getAndParse()
+        .then(function(oldAuthInfo) {
+            if (oldAuthInfo == null) {
+                Authentication.authInfo = new XcAuth({
+                    "idCount": 1
+                });
+                Authentication.kvStore.put(JSON.stringify(Authentication.authInfo), true);
+            } else {
+                delete oldAuthInfo.hashTag;
+                Authentication.authInfo = new XcAuth(oldAuthInfo);
+            }
+
+            deferred.resolve();
+        })
+        .fail(function(error) {
+            console.error("Authentication setup fails", error);
+            deferred.reject(error);
+        });
+
+        return deferred.promise();
+    }
+
+    /**
+     * Authentication.getInfo
+     */
+    public static getInfo(): XcAuth {
+        return Authentication.authInfo;
+    }
+
+    /**
+     * Authentication.getHashId
+     */
+    public static getHashId(): String {
+        const idCount: number = Authentication.authInfo.getIdCount();
+        Authentication.authInfo.incIdCount();
+        Authentication.kvStore.put(JSON.stringify(Authentication.authInfo), true)
+        .fail(function(error) {
+            console.error("Save Authentication fails", error);
+        });
+        return ("#" + idCount);
+    }
+}
