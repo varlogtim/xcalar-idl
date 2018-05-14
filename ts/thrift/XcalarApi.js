a0500ba6a2d9dd3a99f6b73ad65498ac6a239aae
Jerene Yang <jyang@xcalar.com>
2018-05-13 21:13:12 -0700

    GUI-00000 Thrift change
    Change-Id: I99771bcb0cf17ce8c3b248762466d917413b49db

--- ts/thrift/XcalarApi.js
+++ ts/thrift/XcalarApi.js
@@ -4794,9 +4794,6 @@ xcalarApiGetQuery = runEntity.xcalarApiGetQuery = function(thriftHandle, workIte
     case XcalarApisT.XcalarApiSynthesize:
         json["args"] = workItem.input.synthesizeInput;
         break;
-    case XcalarApisT.XcalarApiSelect:
-        json["args"] = workItem.input.selectInput;
-        break;
     default:
         break;
     }
