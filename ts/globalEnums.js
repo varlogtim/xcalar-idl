0014f79bbc2a9e006cc696952eaff2bd9d3c1e88
Cheng Zhang <czhang@xcalar.com>
2018-07-27 16:33:21 -0700

    GUI-0000 More safter gLoginEnabled check
    Change-Id: Idfe06db2180f94ceea9e9a50427c8f4de83c569a

--- ts/globalEnums.js
+++ ts/globalEnums.js
@@ -10,5 +10,5 @@ var Status = {
 /** START DEBUG ONLY **/
 // for XD dev which is on localhost, set to false by default
 // for NON-XD dev which is on VMs,  set to true by default
-var gLoginEnabled = (window.location.hostname === "localhost") ? false : true;
+var gLoginEnabled = (typeof window !== "undefined" && window.location.hostname === "localhost") ? false : true;
 /** END DEBUG ONLY **/
