bcd3efa68eb257da296f755eb40afac0b3001131
Liang Zhang <lzhang@xcalar.com>
2018-08-07 18:15:58 -0700

    GUI-12895 CP1: Change text Jmespath to JMESPath in import form
    Change-Id: I806dcbb7bd7d7ce9301c95507235a6acc843b47e

--- assets/lang/en/htmlTStr.js
+++ assets/lang/en/htmlTStr.js
@@ -370,7 +370,7 @@ module.exports = {
         "RawDSAdvTermAllow": "Stop processing records within a file only when parser completes or cannot continue, and continue to the next file. Report all errors.",
         "RawDSAdvTermStopFile": "Stop importing data as soon as the first file level error is encountered. Record level errors are logged.",
         "RawDSAdvTermStopRecord": "Stop importing data from current file when first error is encountered, and continue to the next file. Report encountered errors.",
-        "RawDSJsonJmespath": "JSON query string. For information about Jmespath, search Jmespath online.",
+        "RawDSJsonJmespath": "JSON query string. For information about JMESPath, search JMESPath online.",
         "RawDSPreviewRefresh": "Click to refresh the preview",
     },
 
@@ -719,7 +719,7 @@ module.exports = {
         "ElementXPath": "Element XPath",
         "DatabaseDSN": "DSN",
         "DatabaseQuery": "SQL",
-        "Jmespath": "Jmespath",
+        "Jmespath": "JMESPath",
         "SelectFile": "Select a file to preview from the dropdown.",
         "PartitionsAdvanced": "Partitions (Advanced)",
         "SelectedColumns": "Selected Columns",
