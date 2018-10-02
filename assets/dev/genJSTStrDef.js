4c02c5bf08316b4e52af0e779e6533a9c402f93c
Liang Zhang <lzhang@xcalar.com>
2018-10-02 12:42:33 -0700

    GUI-13477 Dio: bugfix for jsTStr auto-gen TS Def
    Changes:
    1. Replace Object.entries with Object.keys for better compatibility
    2. Always exit(0), so that not block the grunt process
    
    Change-Id: I380adf1608156b00ab96a462eee25e6196db39b3

--- assets/dev/genJSTStrDef.js
+++ assets/dev/genJSTStrDef.js
@@ -1,9 +1,10 @@
-const fs = require('fs');
 
 try {
+    const fs = require('fs');
+
     if (process.argv.length < 3) {
         console.log('Usage: node genTStrDefinition [inFileName] [outFileName?]');
-        process.exit(1);
+        process.exit(0);
     }
     const inputFileName = process.argv[2];
     const outputFileName = process.argv[3];
@@ -16,26 +17,27 @@ try {
     } else {
         console.log(defStr);
     }
+
+    // Convert string: introTStr = { ... } ==> xcDefs.introTStr = { ... }
+    // Eval to JS function, which returns xcDefs
+    function convertStringToObj(inputStr) {
+        const re = /([\w\d_]+?)\s*?[=]\s*?[{]/g;
+        inputStr = inputStr.replace(re, 'xcDefs.$1 = {');
+        const jsStr = `const xcDefs={};${inputStr}return xcDefs;`;
+        return Function(jsStr)();
+    }
+
+    // Convert xcDefs to "declare namespace xxx { export var fl1: string; .... }"
+    function convertObjToDefstr(obj) {
+        const nsList = [];
+        for (const objName of Object.keys(obj)) {
+            const objValue = obj[objName];
+            const strDefList = Object.keys(objValue).map((field) => `\texport var ${field}: string;`);
+            nsList.push(`declare namespace ${objName} {\n${strDefList.join('\n')}\n}`);
+        }
+        return nsList.join('\n');
+    }
 } catch(e) {
     console.error(e.message || e);
-    process.exit(2);
-}
-
-// Convert string: introTStr = { ... } ==> xcDefs.introTStr = { ... }
-// Eval to JS function, which returns xcDefs
-function convertStringToObj(inputStr) {
-    const re = /([\w\d_]+?)\s*?[=]\s*?[{]/g;
-    inputStr = inputStr.replace(re, 'xcDefs.$1 = {');
-    const jsStr = `const xcDefs={};${inputStr}return xcDefs;`;
-    return Function(jsStr)();
-}
-
-// Convert xcDefs to "declare namespace xxx { export var fl1: string; .... }"
-function convertObjToDefstr(obj) {
-    const nsList = [];
-    for (const [objName, objValue] of Object.entries(obj)) {
-        const strDefList = Object.keys(objValue).map((field) => `\texport var ${field}: string;`);
-        nsList.push(`declare namespace ${objName} {\n${strDefList.join('\n')}\n}`);
-    }
-    return nsList.join('\n');
+    process.exit(0);
 }
