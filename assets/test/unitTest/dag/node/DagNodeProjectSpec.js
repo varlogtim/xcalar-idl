31484ad63d11340bf5fad25fbb77fbff2e50aa19
Liang Zhang <lzhang@xcalar.com>
2019-01-29 16:54:12 -0800

    XD-77 Dio: UnitTest DagNodeProject
    Change-Id: Id4f64408c57b64139dc4f07d7cb50d57aa1d9744

--- assets/test/unitTest/dag/node/DagNodeProjectSpec.js
+++ assets/test/unitTest/dag/node/DagNodeProjectSpec.js
@@ -25,4 +25,25 @@ describe("Project Dag Node Test", () => {
         expect(param).not.to.equal(testParam);
         expect(param).to.deep.equal(testParam);
     });
+
+    it("lineageChange should work", () => {
+        const columns = genProgCols('prefix::col', 3).concat(genProgCols('col', 3));
+        node.setParam({
+            columns: ['prefix::col#1', 'col#1']
+        });
+        
+        const result = node.lineageChange(columns);
+        expect(result.columns.length).to.equal(4);
+        expect(result.changes.length).to.equal(2);
+    });
+
+    function genProgCols(colPrefix, count) {
+        const cols = new Array(count);
+        for (let i = 0; i < count; i ++) {
+            const colName = `${colPrefix}#${i + 1}`;
+            const frontName = xcHelper.parsePrefixColName(colName).name;
+            cols[i] = ColManager.newPullCol(frontName, colName, ColumnType.string);
+        }
+        return cols;
+    }
 });
