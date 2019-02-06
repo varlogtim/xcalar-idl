adcc4ec55fec2f88161680547d68bd2fbc98f079
Steve Watanabe <swatanabe@xcalar.com>
2019-02-06 10:15:59 -0800

    XD-0000 Move hackFunction stubs into shared file
    Instead of replicating much of the hackFunction stubs in both the
    sql and dag util helpers, they've been moved into a separate file which is
    required by the other two.
    
    Also added getType() method to ProgCol in hackFunction to prevent
    run time exception.
    
    Change-Id: Id0842f7a28658f76f6b081e3b55bca6e140e351e

---
+++ services/expServer/utils/hackFunction.js
@@ -0,0 +1,151 @@
+// This file is for stubs for sqlUtils and dagUtils functionality
+
+function hackFunction() {
+
+    // Some browsers and node.js don't support Object.values
+    if (!Object.values) Object.values = o=>Object.keys(o).map(k=>o[k]);
+
+    var CurrentUser = {
+        commitCheck: function() {
+            return PromiseHelper.resolve();
+        }
+    };
+
+    global.XcUser = {
+        CurrentUser: CurrentUser
+    };
+
+    global.TblManager = {
+        setOrphanTableMeta: function() {}
+    };
+
+    global.ColManager = {
+        newPullCol: function(colName, backColName, type) {
+            if (backColName == null) {
+                backColName = colName;
+            }
+            return new ProgCol ( {
+                "backName": backColName,
+                "name": colName,
+                "type": type || null,
+                "width": 100,
+                "isNewCol": false,
+                "userStr": '"' + colName + '" = pull(' + backColName + ')',
+                "func": {
+                    "name": "pull",
+                    "args": [backColName]
+                },
+                "sizedTo": "header"
+            } )
+        },
+
+        newDATACol: function() {
+            return {
+                "backName": "DATA",
+                "name": "DATA",
+                "type": "object",
+                "width": "auto",// to be determined when building table
+                "userStr": "DATA = raw()",
+                "func": {
+                    "name": "raw",
+                    "args": []
+                },
+                "isNewCol": false
+            };
+        }
+    };
+
+    global.Authentication = {
+        getHashId: function() {
+            return xcHelper.randName("#", 8);
+        }
+    };
+
+    global.MonitorGraph = {
+        tableUsageChange: function() {}
+    };
+
+    global.Log = Log = {
+        errorLog: function() { xcConsole.log(arguments); }
+    };
+    global.Admin = Admin = {
+        addNewUser: function(username) {
+            var self = this;
+            var deferred = PromiseHelper.deferred();
+            var kvStore = new KVStore("gUserListKey", gKVScope.GLOB);
+
+            kvStore.get()
+            .then(function(value) {
+                if (value == null) {
+                    xcConsole.log("Adding user to admin panel: " + username);
+                    return self.storeUsername(kvStore, username);
+                } else {
+                    var userList = self.parseStrIntoUserList(value);
+                    // usernames are case sensitive
+                    if (userList.indexOf(username) === -1) {
+                        xcConsole.log("Adding user to admin panel: " + username);
+                        return self.storeUsername(kvStore, username, true);
+                    } else {
+                        xcConsole.log("User exists in admin panel: " + username);
+                        return PromiseHelper.resolve();
+                    }
+                }
+            })
+            .then(deferred.resolve)
+            .fail(function(err) {
+                xcConsole.log(err);
+                deferred.reject(err);
+            });
+
+            return deferred.promise();
+        },
+        storeUsername: function (kvStore, username, append) {
+            var deferred = PromiseHelper.deferred();
+            var entry = JSON.stringify(username) + ",";
+            if (append) {
+                return kvStore.append(entry, true, true);
+            } else {
+                return kvStore.put(entry, true, true);
+            }
+        },
+        parseStrIntoUserList: function (value) {
+            var len = value.length;
+            if (value.charAt(len - 1) === ",") {
+                value = value.substring(0, len - 1);
+            }
+            var arrayStr = "[" + value + "]";
+            var userList;
+            try {
+                userList = JSON.parse(arrayStr);
+            } catch (err) {
+                userList = [];
+                xcConsole.log("Parsing user list failed! ", err);
+            }
+            userList.sort(xcHelper.sortVals);
+            return userList;
+        }
+    };
+
+    class ProgCol {
+        constructor(options) {
+            options = options || {};
+            this.backName = options.backName;
+            this.name = options.name;
+            this.type = options.type || null;
+            this.width = 100;
+            this.isNewCol = false;
+            this.userStr = options.userStr;
+            this.func = options.func;
+            this.sizedTo = options.sizedTo
+        }
+
+        getBackColName() {
+            return this.backName
+        }
+        getType() {
+            return ColumnType.unknown;
+        }
+    };
+}
+
+exports.hackFunction = hackFunction;
