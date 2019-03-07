cb3a207521226a6aa38c2c1b759e0192efd8afa8
Marcel Oyuela-Bonzani <mbonzani@xcalar.com>
2019-03-06 16:44:58 -0800

    XD-56 Unit test for DagNodeAggregate.ts
    Change-Id: I8596ed95e180548ffdeabaeabbd7ee0d22a6e474

--- assets/test/unitTest/dag/node/DagNodeAggregateSpec.js
+++ assets/test/unitTest/dag/node/DagNodeAggregateSpec.js
@@ -1,9 +1,30 @@
 describe("Aggregate Dag Node Test", () => {
     let node;
-    
+    let oldAggManagerAdd;
+    let oldAggManagerHas;
+    let oldAggManagerRemove;
+    let oldAggManagerGet;
+
     before(() => {
-        console.log("Aggregte Dag Node Test");
+        console.log("Aggregate Dag Node Test");
         node = new DagNodeAggregate({});
+        oldAggManagerAdd = DagAggManager.Instance.addAgg;
+        oldAggManagerHas = DagAggManager.Instance.hasAggregate;
+        oldAggManagerRemove = DagAggManager.Instance.removeAgg;
+        oldAggManagerGet = DagAggManager.Instance.getAgg;
+        DagAggManager.Instance.addAgg = function (name, info) {
+            return PromiseHelper.resolve();
+        }
+        DagAggManager.Instance.hasAggregate = function (name) {
+            return false;
+        }
+        DagAggManager.Instance.removeAgg = function (name, force) {
+            return PromiseHelper.resolve();
+        }
+
+        DagAggManager.Instance.getAgg = function(id, name) {
+            return {};
+        }
     });
 
     it("should be an aggregate node", () => {
@@ -30,4 +51,168 @@ describe("Aggregate Dag Node Test", () => {
         expect(param).not.to.equal(testParam);
         expect(param).to.deep.equal(testParam);
     });
+
+    it("Should be able to get and set aggVal", () => {
+        expect(node.getAggVal()).to.equal(null);
+        node.setAggVal(5);
+        expect(node.getAggVal()).to.equal(5);
+        node.setAggVal("cat");
+        expect(node.getAggVal()).to.equal("cat");
+    });
+
+    it("Should be able to get backName", () => {
+        const testParam = {
+            evalString: "count(column)",
+            dest: "constantName",
+            mustExecute: false
+        };
+        node.setParam(testParam);
+        const name = node.getAggBackName();
+        expect(name).to.equal(testParam.dest);
+    });
+
+    it("Should be able to accept a column remapping", () => {
+        const testParam = {
+            evalString: "count(column1)",
+            dest: "constantName",
+            mustExecute: false
+        };
+        node.setParam(testParam);
+
+        node.applyColumnMapping({columns: {
+            "column1": "renamedCol"
+        }});
+
+        param = node.getParam();
+        expect(param.evalString).to.equal("count(renamedCol)")
+    });
+
+    describe("DagAggManager Specific Tests", () => {
+        describe("Should Not Add Aggregates under specific circumstances", () => {
+            it("Should not add an aggregate if the graph is null", () => {
+                let addAggCalled = false;
+                const nodeInfo = {
+                    input: {
+                        evalString: "count(column)",
+                        dest: "constantName",
+                        mustExecute: false
+                    }
+                }
+                DagAggManager.Instance.addAgg = (name, info) => {
+                    addAggCalled = true;
+                    return PromiseHelper.resolve();
+                }
+
+                let testNode = new DagNodeAggregate(nodeInfo);
+
+                UnitTest.testFinish(function(){
+                    return addAggCalled === false;
+                });
+            });
+
+            it("Should not add an aggregate if the aggregate is a sql agg", () => {
+                let addAggCalled = false;
+                const graph = new DagGraph();
+                graph.setTabId("213.sql");
+                const nodeInfo = {
+                    input: {
+                        evalString: "count(column)",
+                        dest: "constantName",
+                        mustExecute: false
+                    },
+                    graph: graph
+                }
+                DagAggManager.Instance.addAgg = (name, info) => {
+                    addAggCalled = true;
+                    return PromiseHelper.resolve();
+                }
+
+                new DagNodeAggregate(nodeInfo);
+
+                UnitTest.testFinish(function(){
+                    return addAggCalled === false;
+                });
+            });
+        });
+
+        it("Should add an aggregate elsewise", () => {
+            let addAggCalled = false;
+            const graph = new DagGraph();
+            graph.setTabId("213");
+            const nodeInfo = {
+                input: {
+                    evalString: "count(column)",
+                    dest: "constantName",
+                    mustExecute: false
+                },
+                graph: graph
+            }
+            DagAggManager.Instance.addAgg = (name, info) => {
+                addAggCalled = true;
+                return PromiseHelper.resolve();
+            }
+
+            new DagNodeAggregate(nodeInfo);
+
+            UnitTest.testFinish(function(){
+                return addAggCalled === true;
+            });
+        });
+
+        it("should delete an aggregate if setting a new aggregate when it previously set one", () => {
+            let addAggCalled = false;
+            let removeAggCalled = false;
+            const testParam = {
+                evalString: "count(column)",
+                dest: "constantName",
+                mustExecute: false
+            };
+            DagAggManager.Instance.addAgg = (name, info) => {
+                addAggCalled = true;
+                return PromiseHelper.resolve();
+            }
+            DagAggManager.Instance.removeAgg = function (name, force) {
+                removeAggCalled = true;
+                return PromiseHelper.resolve();
+            }
+            DagAggManager.Instance.hasAggregate = function (name) {
+                return true;
+            }
+            DagAggManager.Instance.getAgg = function(id, name) {
+                return {
+                    value: 5,
+                    dagName: "oldName"
+                };
+            }
+
+            let newNode = new DagNodeAggregate({});
+
+            newNode.setParam(testParam)
+
+            UnitTest.testFinish(function(){
+                return (addAggCalled === true && removeAggCalled === true);
+            });
+        });
+
+        it("Should have the correct backname when under certain creation circumstances", () => {
+            const nodeInfo = {
+                input: {
+                    evalString: "count(column)",
+                    dest: "constantNameBack",
+                    mustExecute: false
+                }
+            }
+
+            let testNode = new DagNodeAggregate(nodeInfo);
+            expect(testNode.getAggBackName()).to.equal("constantNameBack");
+        });
+    })
+
+    after(() => {
+        DagAggManager.Instance.addAgg = oldAggManagerAdd;
+        DagAggManager.Instance.hasAggregate = oldAggManagerHas;
+        DagAggManager.Instance.removeAgg= oldAggManagerRemove;
+        DagAggManager.Instance.getAgg = oldAggManagerGet;
+    });
+
 });
