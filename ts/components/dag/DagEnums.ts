c7339e115ebe3aaafcd457a8fafd997fd51d443f
Cheng Zhang <czhang@xcalar.com>
2019-04-18 11:09:05 -0700

    XD-1399 Fix a typo
    Change-Id: I3efcbfeb8d58f865bd4b08d437e9633ba117ff31

--- ts/components/dag/DagEnums.ts
+++ ts/components/dag/DagEnums.ts
@@ -70,7 +70,7 @@ enum DagNodeErrorType {
     Disjoint = "Multiple disjoint dataflows detected. Optimized execution can only occur on 1 continuous dataflow.",
     NoColumn = "Invalid column in the schema:\n",
     NoColumns = "Invalid columns in the schema:\n",
-    NoAccessToSource = "User has no rights to accees the dataset",
+    NoAccessToSource = "User has no rights to access the dataset",
     InvalidSQLFunc = "Invalid SQL Function",
 }
 
