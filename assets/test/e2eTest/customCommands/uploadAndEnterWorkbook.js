5238347158340586af7a90acca7306f67d09d446
Rudy Gagneron <rgagneron@xcalar.com>
2019-04-22 13:49:00 -0700

    XD-000 e2e test new user fix
    Change-Id: Ifd9d410c0c975afffee1703e6037844a55cf4d9b

--- assets/test/e2eTest/customCommands/uploadAndEnterWorkbook.js
+++ assets/test/e2eTest/customCommands/uploadAndEnterWorkbook.js
@@ -4,6 +4,7 @@ class UploadAndEnterWorkbook extends EventEmitter {
     command(workbookName, isUpgrade, _cb) {
         this.api
             .uploadWorkbook(workbookName, isUpgrade)
+            .waitForWorkbookReady()
             .activateWorkbook();
         this.emit('complete');
         return this;
