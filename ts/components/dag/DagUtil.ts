54bf61a641ceafbb23844d1ba40f590e93929c53
Cheng Zhang <czhang@xcalar.com>
2019-05-08 15:51:57 -0700

    XD-1179 Fix a dag node in view check bug
    Change-Id: Ibc81e0c981d39c629ff3cecf4e768ac5de1162b0

--- ts/components/dag/DagUtil.ts
+++ ts/components/dag/DagUtil.ts
@@ -16,13 +16,15 @@ class DagUtil {
         toScroll: boolean
     ): boolean {
         try {
-            const matchOffsetLeft: number = $match.offset().left;
+            const offset = $match.offset();
+            const matchOffsetLeft: number = offset.left;
             const bound: ClientRect = $container[0].getBoundingClientRect();
             const leftBoundaray: number = bound.left;
             const rightBoundary: number = bound.right;
             const matchWidth: number = $match.width();
             const matchDiff: number = matchOffsetLeft - (rightBoundary - matchWidth);
 
+            let isInView: boolean = true;
             if (matchDiff > 0 || matchOffsetLeft < leftBoundaray) {
                 if (toScroll) {
                     const scrollLeft: number = $container.scrollLeft();
@@ -30,10 +32,24 @@ class DagUtil {
                     $container.scrollLeft(scrollLeft + matchDiff +
                                             ((viewWidth - matchWidth) / 2));
                 }
-                return false;
-            } else {
-                return true;
+                isInView = false;
             }
+
+            const matchOffSetTop: number = offset.top;
+            const topBoundary: number = bound.top;
+            const bottomBoundary: number = bound.bottom;
+            const matchHeight: number = $match.height();
+            const matchHeightDiff: number = matchOffSetTop - (bottomBoundary - matchHeight);
+            if (matchHeightDiff > 0 || matchOffSetTop < topBoundary) {
+                if (toScroll) {
+                    const scrollTop: number = $container.scrollTop();
+                    const viewHeight: number = $container.height();
+                    $container.scrollTop(scrollTop + matchHeightDiff +
+                                            ((viewHeight - matchHeight) / 2));
+                }
+                isInView = false;
+            }
+            return isInView;
         } catch (e) {
             return false;
         }
