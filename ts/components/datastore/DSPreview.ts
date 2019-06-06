varlogtim/xcalar-idl

commit 47be5d8e54089fd5d284c5fe51210a1b703a27ef

XD-1007 Show disabled state for create dataset button in dsPreview when disabled
Change-Id: I8d10209b1fd835e1e2d33672ae0bfbf5ed45f563

ts/components/datastore/DSPreview.ts
@@ -1436,6 +1436,7 @@ namespace DSPreview {
         $previewWrap.find(".errorSection").addClass("hidden")
                                           .removeClass("cancelState");
         $previewWrap.find(".loadHidden").removeClass("hidden");
+        toggleSubmitButton(false);
     }
 
     function resetPreviewRows(): void {
@@ -3176,7 +3177,7 @@ namespace DSPreview {
 
         let curPreviewId = updatePreviewId();
         let initialLoadArgStr: string;
-        xcUIHelper.disableSubmit($form.find(".confirm"));
+        toggleSubmitButton(true);
         getURLToPreview()
         .then((ret: {sourceIndex: number, url: string}) => {
             const {sourceIndex, url} = ret;
@@ -3373,7 +3374,9 @@ namespace DSPreview {
             deferred.reject(error);
         })
         .always(() => {
-            xcUIHelper.enableSubmit($form.find(".confirm"));
+            if (isValidPreviewId(curPreviewId)) {
+                toggleSubmitButton(false);
+            }
         });
 
         return deferred.promise();
@@ -5554,6 +5557,15 @@ namespace DSPreview {
         let progressCircle = new ProgressCircle(txId, 0, withText);
         $waitSection.find(".cancelLoad").data("progresscircle",
                                                 progressCircle);
+    }
+
+    function toggleSubmitButton(disable: boolean): void {
+        let $btn: JQuery = $form.find(".btn-submit");
+        if (disable) {
+            $btn.addClass("xc-disabled");
+        } else {
+            $btn.removeClass("xc-disabled");
+        }
     }
 
     // currently only being used for CSV

commit 4b6d3836f56c4aa851bc43559397b42b8c1221d0

XD-1007 Fix a always disable preview button bug introduced in 47be5d8e
Change-Id: I7de16dec9d3972a01f6fddbe51fb274b4b87336d

ts/components/datastore/DSPreview.ts
@@ -3370,7 +3370,8 @@ namespace DSPreview {
             deferred.reject(error);
         })
         .always(() => {
-            if (isValidPreviewId(curPreviewId)) {
+            if (previewId == null || isValidPreviewId(curPreviewId)) {
+                // in error case or current preview case
                 toggleSubmitButton(false);
             }
         });