a24fb8880cc82f49ad5c81d0349a597d52901f8b
Rudy Gagneron <rgagneron@xcalar.com>
2019-06-25 18:21:29 -0700

    XD-1076 fix node drag and drop alignment
    Change-Id: If06f390ad0f4e1dec9c39006ea0326a203da5087

--- ts/components/worksheet/dagDrag.ts
+++ ts/components/worksheet/dagDrag.ts
@@ -251,6 +251,7 @@ class DragHelper {
         // find the left most element, right most, top-most, bottom-most
         // so we can create a div that's sized to encapsulate all dragging elements
         // and append these to the div
+        this.$els.find(".topNodeIcon").hide();// prevent nodeIcons from interfering with position
         this.$els.each(function(i) {
             const elOffset = self.elOffsets[i] || {x: 0, y: 0};
             let rect = this.getBoundingClientRect();
@@ -265,6 +266,7 @@ class DragHelper {
             minY = Math.min(minY, top);
             maxY = Math.max(maxY, rect.bottom);
         });
+        this.$els.find(".topNodeIcon").show();
         let width: number = maxX - minX;
         let height: number = maxY - minY;
         const left: number = minX;
