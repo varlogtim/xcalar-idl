483bc0eea54e4419bdf04d0edb8ebf97e1e13596
Jiyuan Li <jli@xcalar.com>
2019-07-23 11:14:04 -0700

    SQL-789 xcrpc JS - Integration test for SessionService
    Change-Id: Icb9183ca40ee46d6c5cec7ebd230932b8dc66dbd

--- assets/test/integrationTest/xcrpc/SessionServiceSpec.js
+++ assets/test/integrationTest/xcrpc/SessionServiceSpec.js
@@ -1,14 +1,135 @@
 const expect = require('chai').expect;
 const ProtoTypes = require('xcalar');
+const ErrorType = require('xcalarsdk').Error.ErrorType;
+const Status = require('xcalarsdk').Error.status;
+const errorStr = require('xcalarsdk').EnumMap.StatusToStr;
+const sessionScope = require('xcalarsdk').Session.SCOPE;
 exports.testSuite = function(SessionService) {
-     describe("SessionService test: ", function () {
-        it("new should work", function () {
-            // XXX TODO: test session service
-            // Need session implemented to access the workbook scope
+    let userName;
+    before(() => {
+        userName = "SessionServiceTestUser_" + new Date().getTime();
+    });
+
+    describe("SessionService test: ", function () {
+        it("new should work", async function () {
+            let sessionName = "SessionServiceTestNew_" + new Date().getTime();
             try {
-                expect(true).to.be.true;
+                let SCOPE = sessionScope.WORKBOOK;
+                let scopeInfo = {
+                    userName: userName,
+                    workbookName: undefined
+                };
+                const sessionId = await SessionService.create({
+                    sessionName: sessionName,
+                    fork: false,
+                    forkedSessionName: "",
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                expect(sessionId).to.not.equal("");
+                // Session delete is not wired in so this session would be left there
             } catch(err) {
+                console.log(err);
                 expect.fail(err);
+            }
+        });
+
+        it("new should work with fork", async function () {
+            let sessionName1 = "SessionServiceTestNewFork_" + new Date().getTime();
+            let sessionName2 = "SessionServiceTestNewFork2_" + new Date().getTime();
+            try {
+                let SCOPE = sessionScope.WORKBOOK;
+                let scopeInfo = {
+                    userName: userName,
+                    workbookName: undefined
+                };
+                const sessionId = await SessionService.create({
+                    sessionName: sessionName1,
+                    fork: false,
+                    forkedSessionName: "",
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                const sessionId2 = await SessionService.create({
+                    sessionName: sessionName2,
+                    fork: true,
+                    forkedSessionName: sessionName1,
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                expect(sessionId).to.not.equal("");
+                expect(sessionId2).to.not.equal("");
+                // Session delete is not wired in so this session would be left there
+            } catch(err) {
+                console.log(err);
+                expect.fail(err);
+            }
+        });
+
+        it("new should return error message on failure", async function () {
+            let sessionName = "SessionServiceTestNewNeg_" + new Date().getTime();
+            let errorName = "SessionNotExist_" + new Date().getTime();
+            try {
+                let SCOPE = sessionScope.WORKBOOK;
+                let scopeInfo = {
+                    userName: userName,
+                    workbookName: undefined
+                };
+                const sessionId = await SessionService.create({
+                    sessionName: sessionName,
+                    fork: true,
+                    forkedSessionName: errorName,
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                expect.fail("Session create should fail with invalid fork session name");
+                // Session delete is not wired in so this session would be left there
+            } catch(err) {
+                expect(err.type).to.equal(ErrorType.XCALAR);
+                expect(err.status).to.equal(Status.STATUS_SESSION_NOT_FOUND);
+                expect(err.error).to.equal(errorStr[Status.STATUS_SESSION_NOT_FOUND]);
+            }
+        });
+
+        it("activate should work", async function () {
+            let sessionName = "SessionServiceTestActivate_" + new Date().getTime();
+            try {
+                let SCOPE = sessionScope.WORKBOOK;
+                let scopeInfo = {
+                    userName: userName,
+                    workbookName: undefined
+                };
+                const sessionId = await SessionService.create({
+                    sessionName: sessionName,
+                    fork: false,
+                    forkedSessionName: "",
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                const response = await SessionService.activate({
+                    sessionName: sessionName,
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                // Session delete is not wired in so this session would be left there
+            } catch(err) {
+                console.log(err);
+                expect.fail(err);
+            }
+        });
+
+        it("activate should return error message on failure", async function () {
+            let errorName = "SessionNotExist_" + new Date().getTime();
+            try {
+                let SCOPE = sessionScope.WORKBOOK;
+                let scopeInfo = {
+                    userName: userName,
+                    workbookName: undefined
+                };
+                const response = await SessionService.activate({
+                    sessionName: errorName,
+                    scope: SCOPE,
+                    scopeInfo: scopeInfo});
+                expect.fail("Session activate should fail with invalid session name");
+                // Session delete is not wired in so this session would be left there
+            } catch(err) {
+                expect(err.type).to.equal(ErrorType.XCALAR);
+                expect(err.status).to.equal(Status.STATUS_SESSION_NOT_FOUND);
+                expect(err.error).to.equal(errorStr[Status.STATUS_SESSION_NOT_FOUND]);
             }
         });
     });
