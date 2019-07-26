a54959c096baad76b706906f8cf1439e035150ee
Cheng Zhang <czhang@xcalar.com>
2019-07-26 14:42:28 -0700

    XD-135 Unit test for SQLOpPanelModel.ts
    Change-Id: I74639285a8328f97081e5cc8149e10a336a93038

---
+++ assets/test/unitTest/worksheet/oppanel/SQLOpPanelModelSpec.js
@@ -0,0 +1,35 @@
+describe("SQLOpPanelModel Test", function() {
+    let model;
+    let node;
+
+    before(function() {
+        node = new DagNodeSQL({});
+        model = new SQLOpPanelModel(node);
+    });
+
+    it("should setDataModel", function() {
+        model.setDataModel(
+            "test",
+            "testTable",
+            [],
+            "testQuery",
+            new Map(),
+            {},
+            true
+        );
+
+        expect(model.getSqlQueryString()).to.equal("test");
+        expect(model.isDropAsYouGo()).to.be.true;
+    });
+
+    it("should setIdentifiers", function() {
+        let map = new Map([[1, "test"], [2, "test2"]]);
+        model.setIdentifiers(map);
+        expect(model.getIdentifiers()).to.equal(map); 
+    });
+
+    it("should sumbit", function() {
+        model.submit();
+        expect(node.getXcQueryString()).to.equal("testQuery");
+    });
+});
