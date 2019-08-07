1acc0c980361ed5bf30bb2fa957c2c5c8c1b2e80
Rudy Gagneron <rgagneron@xcalar.com>
2019-08-07 11:57:02 -0700

    XD-000 close alert modal in e2etest
    Change-Id: I78fa7f6c555c7472def5b6dc24a525370b286b24

--- assets/test/e2eTest/customCommands/activateWorkbook.js
+++ assets/test/e2eTest/customCommands/activateWorkbook.js
@@ -8,19 +8,26 @@ class ActivateWorkbook extends EventEmitter {
         .pause(10000)
         .waitForElementNotVisible("#initialLoadScreen", 100000);
         this.api.isPresent("#intro-popover", (isPresent) => {
-            if (isPresent) {
+            if (isPresent) {// close intro popup if visible
                 this.api.click("#intro-popover .cancel");
                 this.api.pause(1000);
             }
 
-            if (isUpgrade) {
-                this.api.waitForElementVisible("#dagListSection .fileName .name", 10000)
-                .click("#dagListSection .fileName .name");
-            }
+            this.api.isVisible("#alertModal", results => {
+                if (results.value) { // close alert modal if visible
+                    this.api.click("#alertModal .cancel:first-child");
+                    this.api.pause(1000);
+                }
 
-            this.api.waitForElementVisible('.dataflowArea.active.rendered', 100000);
+                if (isUpgrade) {
+                    this.api.waitForElementVisible("#dagListSection .fileName .name", 10000)
+                    .click("#dagListSection .fileName .name");
+                }
 
-            this.emit('complete');
+                this.api.waitForElementVisible('.dataflowArea.active.rendered', 100000);
+
+                this.emit('complete');
+            });
         });
 
         return this;
