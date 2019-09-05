7d75aa2ce43de676ecc1062ce604217eece7c6ec
Sam Davletshin <sdavletshin@xcalar.com>
2019-09-04 18:30:13 -0700

    XD-2273 Use try catch with HTTP requests in cloud login
    Change-Id: I278ba72d7acc42111acea39d8347bcb3d7232506

--- ts/cloudLogin/cloudLogin.js
+++ ts/cloudLogin/cloudLogin.js
@@ -34,14 +34,35 @@ function showInitialScreens() {
     }
 }
 
-// function handleException() {
-//     showInitialScreens()
-// }
+function handleException() {
+    showInitialScreens()
+}
+
+function decodeJwt(token) {
+    try {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace('-', '+').replace('_', '/');
+        const decodedJwt = JSON.parse(window.atob(base64));
+        return decodedJwt;
+    } catch (error) {
+        console.error(error);
+        handleException();
+    }
+}
+
+function getExpiryTime(tokens) {
+    var idToken = tokens["idToken"]
+    if (typeof idToken === "string") {
+        return decodeJwt(idToken)["exp"]
+    } else {
+        return idToken["payload"]["exp"]
+    }
+}
 
 var localTokens = xcLocalStorage.getItem("xcalarTokens");
 
-console.log('localTokens');
-console.log(localTokens);
+// console.log('localTokens');
+// console.log(localTokens);
 
 if (localTokens && localTokens !== "undefined") {
     getCredentials(JSON.parse(localTokens))
@@ -51,113 +72,133 @@ if (localTokens && localTokens !== "undefined") {
 
 async function updateTokens(oldTokens) {
     let newTokens = oldTokens;
-    // try {
     return fetch("https://cognito-idp.us-west-2.amazonaws.com/", {
-        headers: {
-            "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
-            "Content-Type": "application/x-amz-json-1.1",
-        },
-        mode: 'cors',
-        cache: 'no-cache',
-        method: 'POST',
-        body: JSON.stringify({
-            ClientId: clientId,
-            AuthFlow: 'REFRESH_TOKEN_AUTH',
-            AuthParameters: {
-                REFRESH_TOKEN: oldTokens["refreshToken"]["token"],
-            }
-        }),
-    }).then((res) => {
-        console.log(res)
-        return res.json()
-    }).then((result) => {
-        try {
-            console.log(result)
+            headers: {
+                "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth",
+                "Content-Type": "application/x-amz-json-1.1",
+            },
+            mode: 'cors',
+            cache: 'no-cache',
+            method: 'POST',
+            body: JSON.stringify({
+                ClientId: clientId,
+                AuthFlow: 'REFRESH_TOKEN_AUTH',
+                AuthParameters: {
+                    REFRESH_TOKEN: oldTokens["refreshToken"]["token"],
+                }
+            }),
+        })
+        .then(res => res.json())
+        .then(result => {
             newTokens["idToken"] = result.AuthenticationResult.IdToken;
             newTokens["accessToken"] = result.AuthenticationResult.AccessToken;
+            xcLocalStorage.setItem("xcalarTokens", JSON.stringify(newTokens));
             return newTokens;
-        } catch (error) {
-            console.log(error)
-            showInitialScreens()
-        }
-    })
+        })
+        .catch(error => {
+            console.error('updateTokens error:', error);
+            handleException();
+        });
 }
 
-
 async function getCredentials(tokens) {
-    if (Date.now() / 1000 >= tokens["idToken"]["payload"]["exp"]) {
-        // try {
-        console.log('hi')
-        console.log(tokens)
-        tokens = await updateTokens(tokens)
-        console.log(tokens)
-        xcLocalStorage.setItem("xcalarTokens", JSON.stringify(tokens));
-        // } catch (error) {
-        // console.log('failed updateTokens')
-        // console.log(error)
-        // handleException()
-        // }
+    var expiryTime = getExpiryTime(tokens);
+    if (Date.now() / 1000 >= expiryTime) {
+        // if (true) {
+        tokens = await updateTokens(tokens);
     }
     var loginApiUrl = "cognito-idp.us-west-2.amazonaws.com/" + userPoolId;
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: identityPoolId,
-        Logins: {
-            [loginApiUrl]: tokens["idToken"]["jwtToken"]
-        }
-    }, {
-        region: 'us-west-2'
-    });
-    AWS.config.credentials.get(function (err) {
-        if (err) {
-            showInitialScreens()
-        } else {
-            var accessKeyId = AWS.config.credentials.accessKeyId;
-            var secretAccessKey = AWS.config.credentials.secretAccessKey;
-            var sessionToken = AWS.config.credentials.sessionToken;
-            getCluster()
-        }
+
+    try {
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: identityPoolId,
+            Logins: {
+                [loginApiUrl]: tokens["idToken"]["jwtToken"]
+            }
+        }, {
+            region: 'us-west-2'
+        });
+        AWS.config.credentials.get(function (err) {
+            if (err) {
+                console.error('AWS.config.credentials.get error: ', err);
+                showInitialScreens();
+            } else {
+                var accessKeyId = AWS.config.credentials.accessKeyId;
+                var secretAccessKey = AWS.config.credentials.secretAccessKey;
+                var sessionToken = AWS.config.credentials.sessionToken;
+                getCluster();
+            }
+        });
+    } catch (error) {
+        console.error('AWS.config.credentials error: ', error);
+    }
+}
+
+function loginUser(cognitoUser, authenticationDetails) {
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function () {
+            cognitoUser = userPool.getCurrentUser();
+            if (cognitoUser != null) {
+                cognitoUser.getSession(function (err, tokens) {
+                    if (tokens && tokens !== "undefined") {
+                        xcLocalStorage.setItem("xcalarTokens", JSON.stringify(tokens));
+                        getCredentials(tokens);
+                    }
+                });
+            }
+        },
+        onFailure: function (err) {
+            if (err.code === "UserNotConfirmedException") {
+                $("header").children().hide();
+                $("#formArea").children().hide();
+                $("#verifyForm").show();
+                $("#verifyTitle").show();
+            } else {
+                $("#loginFormError").show(); // only for login
+            }
+            console.error(err);
+        },
     });
 }
 
 function getCluster() {
-    var intervalID = window.setInterval(function () {
-        fetch("https://g6sgwgkm1j.execute-api.us-west-2.amazonaws.com/Prod/cluster/get", {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                method: 'POST',
-                body: JSON.stringify({
-                    "username": xcLocalStorage.getItem("xcalarUsername"),
-                    "clusterParams": {
-                        "type": "small"
-                    }
-                }),
-            }).then(response => response.json())
-            .then(clusterGetResponse => {
-                console.log(clusterGetResponse)
-                if (clusterGetResponse.status !== 0 || (clusterGetResponse.isPending === false && clusterGetResponse.clusterUrl === undefined)) {
-                    // go to cluster selection screen
-                    clearInterval(intervalID)
-                    $("header").children().hide()
-                    $("#formArea").children().hide()
-                    $("#clusterTitle").show();
-                    $("#clusterForm").show();
-                } else if (clusterGetResponse.isPending) {
-                    // go to wait screen
-                    $("header").children().hide()
-                    $("#formArea").children().hide()
-                    $("#loadingTitle").show();
-                    $("#loadingForm").show();
-
-                } else {
-                    // redirect to cluster
-                    clearInterval(intervalID)
-                    window.location.href = clusterGetResponse.clusterUrl + "/assets/htmlFiles/login.html?cloud=true";
+    fetch("https://g6sgwgkm1j.execute-api.us-west-2.amazonaws.com/Prod/cluster/get", {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                "username": xcLocalStorage.getItem("xcalarUsername"),
+                "clusterParams": {
+                    "type": "small"
                 }
-            });
-    }, 1000);
+            }),
+        })
+        .then(res => res.json())
+        .then(clusterGetResponse => {
+            if (clusterGetResponse.status !== 0 || (clusterGetResponse.isPending === false && clusterGetResponse.clusterUrl === undefined)) {
+                // go to cluster selection screen
+                $("header").children().hide();
+                $("#formArea").children().hide();
+                $("#clusterTitle").show();
+                $("#clusterForm").show();
+            } else if (clusterGetResponse.isPending) {
+                // go to wait screen
+                setTimeout(getCluster, 3000);
+                $("header").children().hide();
+                $("#formArea").children().hide();
+                $("#loadingTitle").show();
+                $("#loadingForm").show();
+            } else {
+                // redirect to cluster
+                window.location.href = clusterGetResponse.clusterUrl + "/assets/htmlFiles/login.html?cloud=true";
+            }
+        })
+        .catch(error => {
+            console.error('getCluster error:', error);
+            handleException();
+        });
 }
-
 
 function checkLoginForm() {
     var username = document.getElementById("loginNameBox").value;
@@ -168,7 +209,7 @@ function checkLoginForm() {
         }
     } else {
         if ($("#loginButton").hasClass("btn-disabled")) {
-            $("#loginButton").removeClass("btn-disabled")
+            $("#loginButton").removeClass("btn-disabled");
         }
     }
 }
@@ -187,7 +228,7 @@ function checkSignUpForm() {
     var passwordsMatch = $("#signup-email").val() === $("#signup-confirmEmail").val();
     if (filledAllFields && checkedEULA && emailsMatch && passwordsMatch) {
         if ($("#signup-submit").hasClass("btn-disabled")) {
-            $("#signup-submit").removeClass("btn-disabled")
+            $("#signup-submit").removeClass("btn-disabled");
         }
     } else {
         if (!$("#signup-submit").hasClass("btn-disabled")) {
@@ -200,7 +241,7 @@ function btnDisableToggle($input, $btn) {
     $input.keyup(function () {
         if (!($input.val() === "")) {
             if ($btn.hasClass("btn-disabled")) {
-                $btn.removeClass("btn-disabled")
+                $btn.removeClass("btn-disabled");
             }
         } else {
             if (!$btn.hasClass("btn-disabled")) {
@@ -220,7 +261,7 @@ $(".signup-login").click(function () {
 $("#loginForm").find("input").keyup(function () {
     if (fieldsFilled($("#loginForm"))) {
         if ($("#loginButton").hasClass("btn-disabled")) {
-            $("#loginButton").removeClass("btn-disabled")
+            $("#loginButton").removeClass("btn-disabled");
         }
     } else {
         if (!$("#loginButton").hasClass("btn-disabled")) {
@@ -230,11 +271,11 @@ $("#loginForm").find("input").keyup(function () {
 })
 
 $("#signupForm").find(".input").keyup(function () {
-    checkSignUpForm()
+    checkSignUpForm();
 })
 
 $("#signup-termCheck").change(function () {
-    checkSignUpForm()
+    checkSignUpForm();
 })
 
 $("#confirmForgotPasswordForm").find(".input").keyup(function () {
@@ -242,7 +283,7 @@ $("#confirmForgotPasswordForm").find(".input").keyup(function () {
     var passwordsMatch = $("#confirm-forgot-password-new-password").val() === $("#confirm-forgot-password-confirm-new-password").val();
     if (filledAllFields && passwordsMatch) {
         if ($("#confirm-forgot-password-submit").hasClass("btn-disabled")) {
-            $("#confirm-forgot-password-submit").removeClass("btn-disabled")
+            $("#confirm-forgot-password-submit").removeClass("btn-disabled");
         }
     } else {
         if (!$("#confirm-forgot-password-submit").hasClass("btn-disabled")) {
@@ -251,59 +292,33 @@ $("#confirmForgotPasswordForm").find(".input").keyup(function () {
     }
 })
 
-btnDisableToggle($("#verify-code"), $("#verify-submit"))
-btnDisableToggle($("#forgot-password-code"), $("#forgot-password-submit"))
-$("#loginButton").removeClass("btn-disabled")
+btnDisableToggle($("#verify-code"), $("#verify-submit"));
+btnDisableToggle($("#forgot-password-code"), $("#forgot-password-submit"));
+
 $("#loginButton").click(function () {
     username = document.getElementById("loginNameBox").value;
     var password = document.getElementById("loginPasswordBox").value;
-    // username = "sdavletshin@xcalar.com"
-    // var password = "Welcome1!"
 
     var authenticationData = {
         Username: username,
         Password: password,
     };
-    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
     var userData = {
         Username: username,
         Pool: userPool
     };
+
+    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
     cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function () {
-            cognitoUser = userPool.getCurrentUser();
-            if (cognitoUser != null) {
-                cognitoUser.getSession(function (err, tokens) {
-                    if (tokens && tokens !== "undefined") {
-                        console.log('login tokens')
-                        console.log(tokens)
-                        console.log(JSON.stringify(tokens))
-                        xcLocalStorage.setItem("xcalarTokens", JSON.stringify(tokens));
-                        xcLocalStorage.setItem("xcalarUsername", username);
-                        getCredentials(tokens)
-                    }
-                });
-            }
-        },
-        onFailure: function (err) {
-            if (err.code === "UserNotConfirmedException") {
-                $("#loginForm").hide();
-                $("#loginTitle").hide();
-                $("#verifyForm").show();
-                $("#verifyTitle").show();
-            } else {
-                $("#loginFormError").show();
-            }
-            console.log(err);
-        },
-    });
+    xcLocalStorage.setItem("xcalarUsername", username);
+    loginUser(cognitoUser, authenticationDetails);
 });
 
 $("#resend-code").click(function () {
     cognitoUser.resendConfirmationCode(function (err, result) {
         if (err) {
-            console.log(err);
+            console.error(err);
+            alert(err);
             return;
         }
     });
@@ -336,13 +351,11 @@ $("#signup-submit").click(function () {
     attributeList.push(attributeFamilyName);
     attributeList.push(attributeCompany);
 
-
     userPool.signUp(username, password, attributeList, null, function (err, result) {
         if (err) {
-            console.log(err);
+            console.error(err);
             alert(err);
             return;
-
         }
         localPassword = password;
         cognitoUser = result.user;
@@ -358,41 +371,33 @@ $("#verify-submit").click(function () {
     var code = document.getElementById("verify-code").value;
     cognitoUser.confirmRegistration(code, true, function (err, result) {
         if (err) {
+            console.error(err);
             alert(err);
             return;
         }
-
         var authenticationData = {
             Username: xcLocalStorage.getItem("xcalarUsername"),
             Password: localPassword,
         };
         var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
-        cognitoUser.authenticateUser(authenticationDetails, {
-            onSuccess: function () {
-                cognitoUser = userPool.getCurrentUser();
-                if (cognitoUser != null) {
-                    cognitoUser.getSession(function (err, tokens) {
-                        if (tokens && tokens !== "undefined") {
-                            xcLocalStorage.setItem("xcalarTokens", JSON.stringify(tokens));
-                            getCredentials(tokens)
-                        }
-                    });
-                }
-            },
-            onFailure: function (err) {},
-        });
+        if (localPassword) {
+            loginUser(cognitoUser, authenticationDetails);
+        } else {
+            $("header").children().hide();
+            $("#formArea").children().hide();
+            $("#loginTitle").show();
+            $("#loginForm").show();
+        };
     });
 });
 
-
-var selectedClusterSize = "small";
+var selectedClusterSize;
 
 $("#clusterForm").find(".radioButton").click(function () {
-    document.getElementById("deployBtn").classList.remove('btn-disabled')
+    document.getElementById("deployBtn").classList.remove('btn-disabled');
 
     selectedClusterSize = $(this).data('option');
-    console.log(selectedClusterSize);
 
     if ($(this).hasClass("active") || (!$(this).is(":visible"))) {
         return false;
@@ -405,8 +410,6 @@ $("#clusterForm").find(".radioButton").click(function () {
 });
 
 $("#deployBtn").click(function () {
-    console.log('sending /cluster/start')
-
     // start
     fetch("https://g6sgwgkm1j.execute-api.us-west-2.amazonaws.com/Prod/cluster/start", {
             headers: {
@@ -423,8 +426,7 @@ $("#deployBtn").click(function () {
             return response.json();
         })
         .then(function (myJson) {
-            console.log(myJson);
-            getCluster()
+            getCluster();
         });
 });
 
@@ -434,7 +436,6 @@ $("#forgotSection").click(function () {
     $("#forgotPasswordForm").show();
     $("#forgotPasswordTitle").show();
 });
-
 
 $("#forgot-password-submit").click(function () {
     var userData = {
@@ -455,7 +456,6 @@ $("#forgot-password-submit").click(function () {
         }
     });
 });
-
 
 $("#confirm-forgot-password-submit").click(function () {
     var verificationCode = $("#confirm-forgot-password-code").val();
