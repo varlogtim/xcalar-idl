c2468c35df2295778aa2584eb95d0a2b03408430
Cheng Zhang <czhang@xcalar.com>
2019-09-23 13:29:32 -0700

    XD-000 Remove some unused png
    Change-Id: Ia41ffa18f1f6c356c4a60c7b3b567a637340d140

--- ts/shared/setup/paths.ts
+++ ts/shared/setup/paths.ts
@@ -7,11 +7,9 @@ var paths = {
     testAbsolute: '/unitTest.html',
     waitIcon: 'assets/images/waiting-1.gif',
     loadBarIcon: "assets/images/loading-bar.gif",
-    XDExt: 'assets/images/extension/logo-xd.png',
     helpUserGuide: 'assets/help/user/Content/Home.htm',
     helpUserContent: 'assets/help/user/Content/',
     helpUserSearch: 'assets/help/user/Content/SearchInsight.htm?q=',
     favicon: "assets/images/xc-favicon.png",
-    faviconUnsave: "assets/images/xc-favicon-unsave.gif",
     memory: "https://www.xcalar.com/documentation/help/XD/1.3.0/Content/C_AdvancedTasks/I_UnderstandMemoryUse.htm",
 };
