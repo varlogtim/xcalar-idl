1f23ebb4df7566246a9f6ffadf8c56ea66cf2080
Cheng Zhang <czhang@xcalar.com>
2019-10-10 12:42:02 -0700

    ENG-3564 Add circular structure detection code into XDFunc Test
    Change-Id: Iff399ce2209447148a15d1be9d8d18dc21168a04

--- assets/dev/funcTests/states/State.ts
+++ assets/dev/funcTests/states/State.ts
@@ -20,6 +20,9 @@ abstract class State {
 
     public logError(errorMsg: any) {
         if (typeof errorMsg === "object") {
+            if (this.isCyclic(errorMsg)) {
+                console.log("cyclic case!");
+            }
             if (errorMsg instanceof Error) {
                 errorMsg = errorMsg.stack;
             } else if (errorMsg.error instanceof Error) {
@@ -80,4 +83,24 @@ abstract class State {
     /* -------------------------------Helper Function------------------------------- */
 
     abstract takeOneAction(): State;
+
+    private isCyclic(obj) {
+        let seenObjects = [];
+        function detect(obj) {
+            if (obj && typeof obj === 'object') {
+                if (seenObjects.indexOf(obj) !== -1) {
+                    return true;
+                }
+                seenObjects.push(obj);
+                for (let key in obj) {
+                    if (obj.hasOwnProperty(key) && detect(obj[key])) {
+                        console.log(obj + 'cycle at ' + key);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return detect(obj);
+    }
 }
