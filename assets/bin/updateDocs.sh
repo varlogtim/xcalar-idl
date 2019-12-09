f964fb5573d5363d9fedd844bd1df509790db74f
Cheng Zhang <czhang@xcalar.com>
2019-12-09 13:05:01 -0800

    ENG-6095 Fix the updateDocs.sh
    The rm command didn't work so the removing of old files was broken
    
    Change-Id: Ic378c7bab3966abb3f7aa3a4c5a3c9d34d87b9e2

--- assets/bin/updateDocs.sh
+++ assets/bin/updateDocs.sh
@@ -1,6 +1,6 @@
 #please be in xcalar-gui folder first and run ./assets/bin/updateDocs.sh 
-rm -rf ~/xcalar-gui/assets/help/XD/*
+rm -rf assets/help/XD/*
 scp -r jenkins@skywalker.int.xcalar.com:/netstore/doc/help/Internal/XDHelp/* assets/help/XD/
 
-rm -rf ~/xcalar-gui/assets/help/Cloud/*
+rm -rf assets/help/Cloud/*
 scp -r jenkins@skywalker.int.xcalar.com:/netstore/doc/help/Internal/XDHelp_cloud/* assets/help/Cloud/
