8d28fbbf3b0cfbf995ea721574b821ce56a4d29e
Cheng Zhang <czhang@xcalar.com>
2020-02-27 14:47:14 -0800

    ENG-7168 Fix a unit test
    Change-Id: Id383d3bfebe462a6c9cb628e912212659a5517ad

--- assets/test/unitTest/sql/workspace/SQLWorkSpaceSpec.js
+++ assets/test/unitTest/sql/workspace/SQLWorkSpaceSpec.js
@@ -18,18 +18,15 @@ describe("SQLWorkSpace Test", () => {
 
     it("should focus", () => {
         let oldRefresh = SQLWorkSpace.Instance.refresh;
-        let oldToggle = DagViewManager.Instance.toggleSqlPreview;
         let oldShow = SQLResultSpace.Instance.showTables;
         let called = 0;
         SQLWorkSpace.Instance.refresh = () => { called++; };
-        DagViewManager.Instance.toggleSqlPreview = () => { called++; };
         SQLResultSpace.Instance.showTables = () => {};
 
         SQLWorkSpace.Instance.focus();
-        expect(called).not.to.equal(3);
+        expect(called).not.to.equal(2);
 
         SQLWorkSpace.Instance.refresh = oldRefresh;
-        DagViewManager.Instance.toggleSqlPreview = oldToggle;
         SQLResultSpace.Instance.showTables = oldShow;
     });
 });
